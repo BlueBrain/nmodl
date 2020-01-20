19a841d9972b823dc90adb2949a226266306eebf
Omar Awile <omar.awile@epfl.ch>
2020-01-20 15:41:28 +0100

    replaced std::function with lambda
    (and ran clang-format/cmake-format to pass jenkins CI)

--- src/utils/string_utils.hpp
+++ src/utils/string_utils.hpp
@@ -35,24 +35,14 @@ enum class text_alignment { left, right, center };
 
 /// Trim from start
 static inline std::string& ltrim(std::string& s) {
-    s.erase(s.begin(),
-            std::find_if(s.begin(), s.end(), std::not1(
-                std::function<int(int)>(
-                    // NB: the static cast is required to force the compiler to pick this
-                    // version of isspace rather than the one from <locale>
-                    static_cast<int(*)(int)>(std::isspace)))));
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int c) { return !std::isspace(c); }));
     return s;
 }
 
 /// Trim from end
 static inline std::string& rtrim(std::string& s) {
-    s.erase(
-        std::find_if(s.rbegin(), s.rend(), std::not1(
-            std::function<int(int)>(
-                // NB: the static cast is required to force the compiler to pick this
-                // version of isspace rather than the one from <locale>
-                static_cast<int(*)(int)>(std::isspace)))).base(),
-        s.end());
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](int c) { return !std::isspace(c); }).base(),
+            s.end());
     return s;
 }
 
