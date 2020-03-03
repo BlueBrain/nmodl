59c5a661a8c9d325afb5715bda522e8f46204442
katta <alessandro.cattabiani@epfl.ch>
2020-03-03 16:59:44 +0100

    Create the check parent visitor to verify parents
    The visitor goes down the tree keeping track of the parents
    and checking that they are all set-up correctly in the children.

---
+++ src/language/templates/visitors/ckparent_visitor.hpp
@@ -0,0 +1,54 @@
+/*************************************************************************
+ * Copyright (C) 2018-2019 Blue Brain Project
+ *
+ * This file is part of NMODL distributed under the terms of the GNU
+ * Lesser General Public License. See top-level LICENSE file for details.
+ *************************************************************************/
+
+#pragma once
+
+/**
+ *
+ * \dir
+ * \brief Auto generated visitors
+ *
+ * \file
+ * \brief \copybrief nmodl::visitor::CkParentVisitor
+ */
+
+#include "visitors/ast_visitor.hpp"
+#include <stack>
+
+
+namespace nmodl {
+namespace visitor {
+
+/**
+ * @ingroup visitor_classes
+ * @{
+ */
+
+/**
+ * \brief Visitor that checks the parents
+ */
+class CkParentVisitor : public AstVisitor {
+    private:
+        /**
+        * \brief Keeps track of the parents while going down the tree
+        */
+        std::stack<ast::Ast*> lineOfSuccession;
+    public:
+
+
+        {% for node in nodes %}
+        /**
+        * \brief Go through the tree while checking the parents
+        */
+        void visit_{{ node.class_name|snake_case }}(ast::{{ node.class_name }}* node) override;
+        {% endfor %}
+};
+
+/** @} */  // end of visitor_classes
+
+}  // namespace visitor
+}  // namespace nmodl
