73e63dfeaa516f65c97d9e92d427ab947c201e76
Tristan Carel <tristan.carel@epfl.ch>
2020-04-14 10:00:13 +0200

    code_generator: ensure destination has write permissions

--- src/language/code_generator.py
+++ src/language/code_generator.py
@@ -82,6 +82,10 @@ for path in templates_dir.iterdir():
             if clang_format:
                 subprocess.check_call(clang_format + ['-i', tmp_path])
             if not filecmp.cmp(str(destination_file), tmp_path):
+                # ensure destination file has write permissions
+                mode = os.stat(destination_file).st_mode
+                rm_write_mask = stat.S_IWUSR | stat.S_IWGRP | stat.S_IWOTH
+                os.chmod(destination_file, mode | rm_write_mask)
                 shutil.move(tmp_path, destination_file)
                 updated_files.append(str(filepath.name))
         else:
