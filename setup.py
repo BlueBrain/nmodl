bc123b07478d0aa6a679b1b474c9dffabd1dc39d
katta <alessandro.cattabiani@epfl.ch>
2020-04-28 14:15:52 +0200

    scikit-build in charge of python3 setup.py install

--- setup.py
+++ setup.py
@@ -7,18 +7,12 @@
 
 import inspect
 import os
-import os.path as osp
-import platform
-import re
 import subprocess
 import sys
 import sysconfig
-from distutils.version import LooseVersion
 
 from distutils.cmd import Command
-from distutils.dir_util import copy_tree
-from setuptools import Extension, setup
-from setuptools.command.build_ext import build_ext
+from skbuild import setup
 from setuptools.command.test import test
 
 
@@ -54,69 +48,6 @@ class InstallDoc(Command):
         self.run_command("test")
         self.run_command("doctest")
         self.run_command("buildhtml")
-
-class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=""):
-        Extension.__init__(self, name, sources=[])
-        self.sourcedir = osp.abspath(sourcedir)
-
-
-class CMakeBuild(build_ext):
-    def run(self):
-        try:
-            out = subprocess.check_output(["cmake", "--version"])
-        except OSError:
-            raise RuntimeError(
-                "CMake must be installed to build the following extensions: "
-                + ", ".join(e.name for e in self.extensions)
-            )
-
-        cmake_version = LooseVersion(
-            re.search(r"version\s*([\d.]+)", out.decode()).group(1)
-        )
-        if cmake_version < "3.3.0":
-            raise RuntimeError("CMake >= 3.3.0 is required")
-
-        for ext in self.extensions:
-            self.build_extension(ext)
-
-    def build_extension(self, ext):
-        extdir = osp.abspath(osp.dirname(self.get_ext_fullpath(ext.name)))
-        extdir = osp.join(extdir, ext.name)
-        cmake_args = [
-            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
-            "-DPYTHON_EXECUTABLE=" + sys.executable,
-        ]
-
-        cfg = "Debug" if self.debug else "Release"
-        build_args = ["--config", cfg]
-
-        if platform.system() == "Windows":
-            cmake_args += [
-                "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)
-            ]
-            if sys.maxsize > 2 ** 32:
-                cmake_args += ["-A", "x64"]
-            build_args += ["--", "/m"]
-        else:
-            cmake_args += ["-DCMAKE_BUILD_TYPE=" + cfg]
-            build_args += ["--", "-j{}".format(max(1, os.cpu_count() - 3))]
-
-        env = os.environ.copy()
-        env["CXXFLAGS"] = '{} -DVERSION_INFO=\\"{}\\"'.format(
-            env.get("CXXFLAGS", ""), self.distribution.get_version()
-        )
-        if not osp.exists(self.build_temp):
-            os.makedirs(self.build_temp)
-        subprocess.check_call(
-            ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env
-        )
-        subprocess.check_call(
-            ["cmake", "--build", "."] + build_args, cwd=self.build_temp
-        )
-
-        # copy nmodl module with shared library to extension directory
-        copy_tree(os.path.join(self.build_temp, 'nmodl'), extdir)
 
 class NMODLTest(test):
     """Custom disutils command that acts like as a replacement
@@ -158,9 +89,9 @@ setup(
     long_description="",
     packages=["nmodl"],
     include_package_data=True,
-    ext_modules=[CMakeExtension("nmodl")],
+    cmake_minimum_required_version="3.3.0",
+    cmake_args=["-DPYTHON_EXECUTABLE=" + sys.executable],
     cmdclass=lazy_dict(
-        build_ext=CMakeBuild,
         test=NMODLTest,
         install_doc=InstallDoc,
         doctest=get_sphinx_command,
