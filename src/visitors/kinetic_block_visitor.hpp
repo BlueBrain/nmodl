a5f78c6114a8efc04323f67888c9b0160160458a
Ioannis Magkanaris <iomagkanaris@gmail.com>
2020-05-29 19:51:06 +0200

    Fixed some headers

--- src/visitors/kinetic_block_visitor.hpp
+++ src/visitors/kinetic_block_visitor.hpp
@@ -15,6 +15,7 @@
 #include <memory>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 #include "visitors/ast_visitor.hpp"
--- src/visitors/sympy_solver_visitor.hpp
+++ src/visitors/sympy_solver_visitor.hpp
@@ -14,8 +14,8 @@
 
 #include <pybind11/embed.h>
 #include <pybind11/stl.h>
-#include <set>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 #include "ast/ast.hpp"
