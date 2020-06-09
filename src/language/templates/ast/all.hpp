315db545d96823c20dae2f487e3989f524855690
Ioannis Magkanaris <iomagkanaris@gmail.com>
2020-06-09 12:14:10 +0200

    Ast generated code: fix template members / move definitions in compilation unit
    * In `ast/all.hpp` the definition of template member functions are
    now after the definition of all Ast classes.
    * The definition of some member methods has been moved in `ast/ast.cpp`
    compilation units instead of inlining them.

--- src/language/templates/ast/all.hpp
+++ src/language/templates/ast/all.hpp
@@ -20,13 +20,25 @@
  */
 
 #include "ast/ast.hpp"
-{% for header in node_info.CIRCULAR_DEPENDECIES %}
-#include "ast/{{header}}.hpp"
-{% endfor %}
 
 {% for node in nodes %}
 #ifndef {{ node.cpp_fence }}
 #define {{ node.cpp_fence }}
+#define {{ node.cpp_fence }}_INLINE_DEFINITION_REQUIRED
 {% include "ast/node_class.template" %}
 #endif // !{{ node.cpp_fence }}
 {% endfor %}
+
+{# add inline definitions of template member methods #}
+namespace nmodl {
+namespace ast {
+{% for node in nodes %}
+{%- if node.has_template_methods %}
+#ifdef {{ node.cpp_fence }}_INLINE_DEFINITION_REQUIRED
+  {% include "ast/node_class_inline_definition.template" %}
+#endif  // !{{ node.cpp_fence }}_INLINE_DEFINITION_REQUIRED
+
+{% endif %}
+{%- endfor %}
+}  // namespace ast
+}  // namespace nmodl
