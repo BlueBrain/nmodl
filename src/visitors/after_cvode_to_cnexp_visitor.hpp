30f88feae940f7a25facdae7134b2254f7ea82e4
Ioannis Magkanaris <iomagkanaris@gmail.com>
2020-10-05 11:26:33 +0200

    Update src/visitors/after_cvode_to_cnexp_visitor.hpp
    Co-authored-by: Pramod Kumbhar <pramod.kumbhar@epfl.ch>

--- src/visitors/after_cvode_to_cnexp_visitor.hpp
+++ src/visitors/after_cvode_to_cnexp_visitor.hpp
@@ -31,7 +31,9 @@ namespace visitor {
  * \class AfterCVodeToCnexpVisitor
  * \brief Visitor to change usage of after_cvode solver to cnexp
  *
-
+ *  `CVODE` is not supported in CoreNEURON. If MOD file has `after_cvode` solver then
+ *   we can treat that as `cnexp`. In order to re-use existing passes, in this visitor we
+ *  replace `after_cvode` with `cnexp`. 
  */
 
 class AfterCVodeToCnexpVisitor: public AstVisitor {
