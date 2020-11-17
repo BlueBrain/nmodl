80531e236ef1c57727d112137d8dcf5c89696e84
Alessandro Cattabiani <cattabiani@users.noreply.github.com>
2020-11-17 14:25:10 +0100

    Update nmodl/__init__.py
    Co-authored-by: Omar Awile <omar.awile@epfl.ch>

--- nmodl/__init__.py
+++ nmodl/__init__.py
@@ -1,8 +1,7 @@
 try:
-# check if nmodl binds are built before importing
+    # Try importing but catch exception in case bindings are not available
     from ._nmodl import NmodlDriver, to_json, to_nmodl  # noqa
     from ._nmodl import __version__
     __all__ = ["NmodlDriver", "to_json", "to_nmodl"]
 except ImportError:
     print("[NMODL] [warning] :: Python bindings are not available")
-
