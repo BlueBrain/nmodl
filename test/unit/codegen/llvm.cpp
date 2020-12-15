e3ef5ecfe14322a4ad91664276cf25d7f6f3bcf2
George Mitenkov <georgemitenk0v@gmail.com>
2020-12-15 14:41:17 +0300

    Added unit tests for binary and unary operators

--- test/unit/codegen/llvm.cpp
+++ test/unit/codegen/llvm.cpp
@@ -36,6 +36,88 @@ std::string run_llvm_visitor(const std::string& text) {
 }
 
 //=============================================================================
+// BinaryExpression and Double
+//=============================================================================
+
+SCENARIO("Binary expression", "[visitor][llvm]") {
+    GIVEN("Procedure with addition of its arguments") {
+        std::string nmodl_text = R"(
+            PROCEDURE add(a, b) {
+                LOCAL i
+                i = a + b
+            }
+        )";
+
+        THEN("variables are loaded and add instruction is created") {
+            std::string module_string = run_llvm_visitor(nmodl_text);
+            std::smatch m;
+
+            // Check the values are loaded correctly and added
+            std::regex rhs(R"(%1 = load double, double\* %b)");
+            std::regex lhs(R"(%2 = load double, double\* %a)");
+            std::regex res(R"(%3 = fadd double %2, %1)");
+            REQUIRE(std::regex_search(module_string, m, rhs));
+            REQUIRE(std::regex_search(module_string, m, lhs));
+            REQUIRE(std::regex_search(module_string, m, res));
+        }
+    }
+
+    GIVEN("Procedure with multiple binary operators") {
+        std::string nmodl_text = R"(
+            PROCEDURE multiple(a, b) {
+                LOCAL i
+                i = (a - b) / (a + b)
+            }
+        )";
+
+        THEN("variables are processed from rhs first") {
+            std::string module_string = run_llvm_visitor(nmodl_text);
+            std::smatch m;
+
+            // Check rhs
+            std::regex rr(R"(%1 = load double, double\* %b)");
+            std::regex rl(R"(%2 = load double, double\* %a)");
+            std::regex x(R"(%3 = fadd double %2, %1)");
+            REQUIRE(std::regex_search(module_string, m, rr));
+            REQUIRE(std::regex_search(module_string, m, rl));
+            REQUIRE(std::regex_search(module_string, m, x));
+
+            // Check lhs
+            std::regex lr(R"(%4 = load double, double\* %b)");
+            std::regex ll(R"(%5 = load double, double\* %a)");
+            std::regex y(R"(%6 = fsub double %5, %4)");
+            REQUIRE(std::regex_search(module_string, m, lr));
+            REQUIRE(std::regex_search(module_string, m, ll));
+            REQUIRE(std::regex_search(module_string, m, y));
+
+            // Check result
+            std::regex res(R"(%7 = fdiv double %6, %3)");
+            REQUIRE(std::regex_search(module_string, m, res));
+        }
+    }
+
+    GIVEN("Procedure with assignment") {
+        std::string nmodl_text = R"(
+            PROCEDURE assignment() {
+                LOCAL i
+                i = 2
+            }
+        )";
+
+        THEN("double constant is stored into i") {
+            std::string module_string = run_llvm_visitor(nmodl_text);
+            std::smatch m;
+
+            // Check store immediate is created
+            std::regex allocation(R"(%i = alloca double)");
+            std::regex assignment(R"(store double 2.0*e\+00, double\* %i)");
+            REQUIRE(std::regex_search(module_string, m, allocation));
+            REQUIRE(std::regex_search(module_string, m, assignment));
+        }
+    }
+}
+
+//=============================================================================
 // LocalList and LocalVar
 //=============================================================================
 
@@ -98,6 +180,31 @@ SCENARIO("Procedure", "[visitor][llvm]") {
             std::regex store_instr(R"(store double %x1, double\* %x)");
             REQUIRE(std::regex_search(module_string, m, alloca_instr));
             REQUIRE(std::regex_search(module_string, m, store_instr));
+        }
+    }
+}
+
+//=============================================================================
+// UnaryExpression
+//=============================================================================
+
+SCENARIO("Unary expression", "[visitor][llvm]") {
+    GIVEN("Procedure with negation") {
+        std::string nmodl_text = R"(
+            PROCEDURE negation(a) {
+                LOCAL i
+                i = -a
+            }
+        )";
+
+        THEN("fneg instruction is created") {
+            std::string module_string = run_llvm_visitor(nmodl_text);
+            std::smatch m;
+
+            std::regex allocation(R"(%1 = load double, double\* %a)");
+            std::regex negation(R"(fneg double %1)");
+            REQUIRE(std::regex_search(module_string, m, allocation));
+            REQUIRE(std::regex_search(module_string, m, negation));
         }
     }
 }
