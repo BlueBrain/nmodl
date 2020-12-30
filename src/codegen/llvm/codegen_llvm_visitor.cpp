438dca865e8b3070e37a6abf0c9c3c86e39843e7
George Mitenkov <georgemitenk0v@gmail.com>
2020-12-31 00:49:13 +0300

    Support for IndexedName codegen (#478)
    LLVM code generation for `IndexedName`s.
    
    - Added code generation for initialising arrays in LOCAL blocks (with both integer constants and macros).
    - Added support for indexing arrays.
    
    fixes #467

--- src/codegen/llvm/codegen_llvm_visitor.cpp
+++ src/codegen/llvm/codegen_llvm_visitor.cpp
@@ -25,6 +25,44 @@ namespace codegen {
 /*                            Helper routines                                           */
 /****************************************************************************************/
 
+bool CodegenLLVMVisitor::check_array_bounds(const ast::IndexedName& node, unsigned index) {
+    llvm::Type* array_type =
+        local_named_values->lookup(node.get_node_name())->getType()->getPointerElementType();
+    unsigned length = array_type->getArrayNumElements();
+    return 0 <= index && index < length;
+}
+
+llvm::Value* CodegenLLVMVisitor::create_gep(const std::string& name, unsigned index) {
+    llvm::Type* index_type = llvm::Type::getInt32Ty(*context);
+    std::vector<llvm::Value*> indices;
+    indices.push_back(llvm::ConstantInt::get(index_type, 0));
+    indices.push_back(llvm::ConstantInt::get(index_type, index));
+
+    return builder.CreateInBoundsGEP(local_named_values->lookup(name), indices);
+}
+
+llvm::Value* CodegenLLVMVisitor::codegen_indexed_name(const ast::IndexedName& node) {
+    unsigned index = get_array_index_or_length(node);
+
+    // Check if index is within array bounds.
+    if (!check_array_bounds(node, index))
+        throw std::runtime_error("Error: Index is out of bounds");
+
+    return create_gep(node.get_node_name(), index);
+}
+
+unsigned CodegenLLVMVisitor::get_array_index_or_length(const ast::IndexedName& indexed_name) {
+    auto integer = std::dynamic_pointer_cast<ast::Integer>(indexed_name.get_length());
+    if (!integer)
+        throw std::runtime_error("Error: expecting integer index or length");
+
+    // Check if integer value is taken from a macro.
+    if (!integer->get_macro())
+        return integer->get_value();
+    const auto& macro = sym_tab->lookup(integer->get_macro()->get_node_name());
+    return static_cast<unsigned>(*macro->get_value());
+}
+
 void CodegenLLVMVisitor::run_llvm_opt_passes() {
     /// run some common optimisation passes that are commonly suggested
     fpm.add(llvm::createInstructionCombiningPass());
@@ -42,7 +80,6 @@ void CodegenLLVMVisitor::run_llvm_opt_passes() {
         fpm.run(function);
     }
 }
-
 
 void CodegenLLVMVisitor::create_external_method_call(const std::string& name,
                                                      const ast::ExpressionVector& arguments) {
@@ -187,8 +224,17 @@ void CodegenLLVMVisitor::visit_binary_expression(const ast::BinaryExpression& no
         if (!var) {
             throw std::runtime_error("Error: only VarName assignment is currently supported.\n");
         }
-        llvm::Value* alloca = local_named_values->lookup(var->get_node_name());
-        builder.CreateStore(rhs, alloca);
+
+        const auto& identifier = var->get_name();
+        if (identifier->is_name()) {
+            llvm::Value* alloca = local_named_values->lookup(var->get_node_name());
+            builder.CreateStore(rhs, alloca);
+        } else if (identifier->is_indexed_name()) {
+            auto indexed_name = std::dynamic_pointer_cast<ast::IndexedName>(identifier);
+            builder.CreateStore(rhs, codegen_indexed_name(*indexed_name));
+        } else {
+            throw std::runtime_error("Error: Unsupported variable type");
+        }
         return;
     }
 
@@ -254,10 +300,22 @@ void CodegenLLVMVisitor::visit_integer(const ast::Integer& node) {
 
 void CodegenLLVMVisitor::visit_local_list_statement(const ast::LocalListStatement& node) {
     for (const auto& variable: node.get_variables()) {
-        // LocalVar always stores a Name.
-        auto name = variable->get_node_name();
-        llvm::Type* var_type = llvm::Type::getDoubleTy(*context);
-        llvm::Value* alloca = builder.CreateAlloca(var_type, /*ArraySize=*/nullptr, name);
+        std::string name = variable->get_node_name();
+        const auto& identifier = variable->get_name();
+        // Local variable can be a scalar (Node AST class) or an array (IndexedName AST class). For
+        // each case, create memory allocations with the corresponding LLVM type.
+        llvm::Type* var_type;
+        if (identifier->is_indexed_name()) {
+            auto indexed_name = std::dynamic_pointer_cast<ast::IndexedName>(identifier);
+            unsigned length = get_array_index_or_length(*indexed_name);
+            var_type = llvm::ArrayType::get(llvm::Type::getDoubleTy(*context), length);
+        } else if (identifier->is_name()) {
+            // This case corresponds to a scalar local variable. Its type is double by default.
+            var_type = llvm::Type::getDoubleTy(*context);
+        } else {
+            throw std::runtime_error("Error: Unsupported local variable type");
+        }
+        builder.CreateAlloca(var_type, /*ArraySize=*/nullptr, name);
     }
 }
 
@@ -310,7 +368,21 @@ void CodegenLLVMVisitor::visit_unary_expression(const ast::UnaryExpression& node
 }
 
 void CodegenLLVMVisitor::visit_var_name(const ast::VarName& node) {
-    llvm::Value* var = builder.CreateLoad(local_named_values->lookup(node.get_node_name()));
+    const auto& identifier = node.get_name();
+    if (!identifier->is_name() && !identifier->is_indexed_name())
+        throw std::runtime_error("Error: Unsupported variable type");
+
+    llvm::Value* ptr;
+    if (identifier->is_name())
+        ptr = local_named_values->lookup(node.get_node_name());
+
+    if (identifier->is_indexed_name()) {
+        auto indexed_name = std::dynamic_pointer_cast<ast::IndexedName>(identifier);
+        ptr = codegen_indexed_name(*indexed_name);
+    }
+
+    // Finally, load the variable from the pointer value.
+    llvm::Value* var = builder.CreateLoad(ptr);
     values.push_back(var);
 }
 
