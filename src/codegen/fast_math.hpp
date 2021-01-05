8cdeab26ef6b140cdf8820f1ee15ed6fd5b808c8
katta <alessandro.cattabiani@gmail.com>
2021-01-05 15:59:58 +0100

    Add fast_math for non-ispc compilers too

--- src/codegen/fast_math.hpp
+++ src/codegen/fast_math.hpp
@@ -10,76 +10,78 @@
  * exprelr and expm1 are based on https://arbor.readthedocs.io/en/latest/internals/simd_api.html?highlight=exprelr#operatorname-expm1-x
  *************************************************************************/
 
+#pragma once
+
 /**
  * \file
  * \brief Implementation of different math functions with ISPC
  */
 
-static inline double uint642dp(unsigned int64 ll) {
-    return *((varying double*) (&ll));
+static inline double uint642dp(u_int64_t ll) {
+    return *(( double*) (&ll));
 }
 
-static inline unsigned int64 dp2uint64(double x) {
-    return *((varying unsigned int64*) (&x));
+static inline u_int64_t dp2uint64(double x) {
+    return *(( u_int64_t*) (&x));
 }
 
-static inline float uint322sp(int32 x) {
-    return *((varying float*) (&x));
+static inline float uint322sp(int32_t x) {
+    return *(( float*) (&x));
 }
 
 static inline unsigned int sp2uint32(float x) {
-    return *((varying unsigned int32*) (&x));
+    return *(( u_int32_t*) (&x));
 }
 
 static inline double dpfloor(const double x) {
-    int32 ret = x;
+    int32_t ret = x;
     ret -= (sp2uint32(x) >> 31);
     return ret;
 }
 
 static inline float spfloor(const float x) {
-    int32 ret = x;
+    int32_t ret = x;
     ret -= (sp2uint32(x) >> 31);
     return ret;
 }
 
-static inline uniform float f_inf() {
-    uniform unsigned int32 v = 0x7F800000;
+static inline float f_inf() {
+    u_int32_t v = 0x7F800000;
     return *((float*) (&v));
 }
 
-static inline uniform double inf() {
-    uniform unsigned int64 v = 0x7FF0000000000000;
+static inline double inf() {
+    u_int64_t v = 0x7FF0000000000000;
     return *((double*) (&v));
 }
 
 
-static const uniform double EXP_LIMIT = 708.d;
+static const double EXP_LIMIT = 708.0;
 
-static const uniform double PX1exp = 1.26177193074810590878d-4;
-static const uniform double PX2exp = 3.02994407707441961300d-2;
-static const uniform double PX3exp = 9.99999999999999999910d-1;
-static const uniform double QX1exp = 3.00198505138664455042d-6;
-static const uniform double QX2exp = 2.52448340349684104192d-3;
-static const uniform double QX3exp = 2.27265548208155028766d-1;
-static const uniform double QX4exp = 2.00000000000000000009d;
+static const double PX1exp = 1.26177193074810590878-4;
+static const double PX2exp = 3.02994407707441961300-2;
+static const double PX3exp = 9.99999999999999999910-1;
+static const double QX1exp = 3.00198505138664455042-6;
+static const double QX2exp = 2.52448340349684104192-3;
+static const double QX3exp = 2.27265548208155028766-1;
+static const double QX4exp = 2.00000000000000000009;
 
-static const uniform double LOG2E = 1.4426950408889634073599d; // 1/ln(2)
+static const double LOG2E = 1.4426950408889634073599; // 1/ln(2)
 
-static const uniform float MAXLOGF = 88.72283905206835f;
-static const uniform float MINLOGF = -88.f;
+static const float MAXLOGF = 88.72283905206835f;
+static const float MINLOGF = -88.f;
 
-static const uniform float C1F = 0.693359375f;
-static const uniform float C2F = -2.12194440e-4f;
+static const float C1F = 0.693359375f;
+static const float C2F = -2.12194440e-4f;
 
-static const uniform float PX1expf = 1.9875691500E-4f;
-static const uniform float PX2expf = 1.3981999507E-3f;
-static const uniform float PX3expf = 8.3334519073E-3f;
-static const uniform float PX4expf = 4.1665795894E-2f;
-static const uniform float PX5expf = 1.6666665459E-1f;
-static const uniform float PX6expf = 5.0000001201E-1f;
+static const float PX1expf = 1.9875691500E-4f;
+static const float PX2expf = 1.3981999507E-3f;
+static const float PX3expf = 8.3334519073E-3f;
+static const float PX4expf = 4.1665795894E-2f;
+static const float PX5expf = 1.6666665459E-1f;
+static const float PX6expf = 5.0000001201E-1f;
 
-static const uniform float LOG2EF = 1.44269504088896341f; // 1/ln(2)
+static const float LOG2EF = 1.44269504088896341f; // 1/ln(2)
 
 static inline double egm1(double x, double px) {
 
@@ -109,16 +111,16 @@ static inline double egm1(double x, double px) {
 /// double precision exp function
 static inline double vexp(double initial_x) {
     double x = initial_x;
-    double px = dpfloor(LOG2E * x + 0.5d);
-    const int32 n = px;
+    double px = dpfloor(LOG2E * x + 0.5);
+    const int32_t n = px;
 
-    x = 1.0d + egm1(x, px);
-    x *= uint642dp((((unsigned int64) n) + 1023) << 52);
+    x = 1.0 + egm1(x, px);
+    x *= uint642dp((((u_int64_t) n) + 1023) << 52);
 
     if (initial_x > EXP_LIMIT)
         x = inf();
     if (initial_x < -EXP_LIMIT)
-        x = 0.d;
+        x = 0.0;
 
     return x;
 }
@@ -126,12 +128,12 @@ static inline double vexp(double initial_x) {
 /// double precision expm1 function
 static inline double vexpm1(double initial_x) {
     double x = initial_x;
-    double px = dpfloor(LOG2E * x + 0.5d);
+    double px = dpfloor(LOG2E * x + 0.5);
 
-    const int32 n = ((px > 0.5) || (px >= -0.5)) ? px : 0;
+    const int32_t n = ((px > 0.5) || (px >= -0.5)) ? px : 0;
 
     x = egm1(x, px);
-    const double pow2nm1 = uint642dp((((unsigned int64) n-1) + 1023) << 52);
+    const double pow2nm1 = uint642dp((((u_int64_t) n-1) + 1023) << 52);
     x *= pow2nm1;
     x += pow2nm1-1;
     x *= 2;
@@ -139,7 +141,7 @@ static inline double vexpm1(double initial_x) {
     if (initial_x > EXP_LIMIT)
         x = inf();
     if (initial_x < -EXP_LIMIT)
-        x = -1.d;
+        x = -1.0;
 
     return x;
 }
@@ -178,7 +180,7 @@ static inline float vexp(float initial_x) {
 
     x -= z * C1F;
     x -= z * C2F;
-    const int32 n = z;
+    const int32_t n = z;
 
     z = 1.0f + egm1(x);
 
@@ -199,7 +201,7 @@ static inline float vexpm1(float initial_x) {
 
     x -= z * C1F;
     x -= z * C2F;
-    const int32 n = z;
+    const int32_t n = z;
 
     z = egm1(x);
     const double pow2nm1 = uint322sp((n-1 + 0x7f) << 23);
@@ -223,6 +225,3 @@ static inline float exprelr(float initial_x) {
 
     return initial_x/vexpm1(initial_x);
 }
-
-
-
