df18714b1c13769abca1a556792d97b4b9c93398
George Mitenkov <georgemitenk0v@gmail.com>
2021-01-05 10:07:24 +0300

    JIT runner main set-up

---
+++ src/codegen/llvm/main.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+ * Copyright (C) 2018-2019 Blue Brain Project
+ *
+ * This file is part of NMODL distributed under the terms of the GNU
+ * Lesser General Public License. See top-level LICENSE file for details.
+ *************************************************************************/
+
+#include <CLI/CLI.hpp>
+
+#include "utils/logger.hpp"
+
+using namespace nmodl;
+
+int main(int argc, const char* argv[]) {
+    logger->info("Hello world");
+    return 0;
+}
