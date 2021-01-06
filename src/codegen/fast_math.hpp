d8e2bf6c7a89af444ba9334bffe58c8f2ea61870
katta <alessandro.cattabiani@gmail.com>
2021-01-06 12:52:47 +0100

    Add unit tests

--- src/codegen/fast_math.hpp
+++ src/codegen/fast_math.hpp
@@ -7,30 +7,33 @@
  * Note that the fast ISPC exponentials Based on VDT implementation of
  * D. Piparo et al. See https://github.com/dpiparo/vdt for additional
  * license information.
- * exprelr and expm1 are based on https://arbor.readthedocs.io/en/latest/internals/simd_api.html?highlight=exprelr#operatorname-expm1-x
+ * exprelr and expm1 are based on
+ *https://arbor.readthedocs.io/en/latest/internals/simd_api.html?highlight=exprelr#operatorname-expm1-x
  *************************************************************************/
 
 #pragma once
+
+#include <stdint.h>
 
 /**
  * \file
  * \brief Implementation of different math functions with ISPC
  */
 
-static inline double uint642dp(u_int64_t ll) {
-    return *(( double*) (&ll));
+static inline double uint642dp(uint64_t ll) {
+    return *((double*) (&ll));
 }
 
-static inline u_int64_t dp2uint64(double x) {
-    return *(( u_int64_t*) (&x));
+static inline uint64_t dp2uint64(double x) {
+    return *((uint64_t*) (&x));
 }
 
 static inline float uint322sp(int32_t x) {
-    return *(( float*) (&x));
+    return *((float*) (&x));
 }
 
 static inline unsigned int sp2uint32(float x) {
-    return *(( u_int32_t*) (&x));
+    return *((uint32_t*) (&x));
 }
 
 static inline double dpfloor(const double x) {
@@ -46,27 +49,27 @@ static inline float spfloor(const float x) {
 }
 
 static inline float f_inf() {
-    u_int32_t v = 0x7F800000;
+    uint32_t v = 0x7F800000;
     return *((float*) (&v));
 }
 
 static inline double inf() {
-    u_int64_t v = 0x7FF0000000000000;
+    uint64_t v = 0x7FF0000000000000;
     return *((double*) (&v));
 }
 
 
 static const double EXP_LIMIT = 708.0;
 
-static const double PX1exp = 1.26177193074810590878-4;
-static const double PX2exp = 3.02994407707441961300-2;
-static const double PX3exp = 9.99999999999999999910-1;
-static const double QX1exp = 3.00198505138664455042-6;
-static const double QX2exp = 2.52448340349684104192-3;
-static const double QX3exp = 2.27265548208155028766-1;
+static const double PX1exp = 1.26177193074810590878E-4;
+static const double PX2exp = 3.02994407707441961300E-2;
+static const double PX3exp = 9.99999999999999999910E-1;
+static const double QX1exp = 3.00198505138664455042E-6;
+static const double QX2exp = 2.52448340349684104192E-3;
+static const double QX3exp = 2.27265548208155028766E-1;
 static const double QX4exp = 2.00000000000000000009;
 
-static const double LOG2E = 1.4426950408889634073599; // 1/ln(2)
+static const double LOG2E = 1.4426950408889634073599;  // 1/ln(2)
 
 static const float MAXLOGF = 88.72283905206835f;
 static const float MINLOGF = -88.f;
@@ -81,12 +84,11 @@ static const float PX4expf = 4.1665795894E-2f;
 static const float PX5expf = 1.6666665459E-1f;
 static const float PX6expf = 5.0000001201E-1f;
 
-static const float LOG2EF = 1.44269504088896341f; // 1/ln(2)
+static const float LOG2EF = 1.44269504088896341f;  // 1/ln(2)
 
 static inline double egm1(double x, double px) {
-
-    x -= px * 6.93145751953125d-1;
-    x -= px * 1.42860682030941723212d-6;
+    x -= px * 6.93145751953125E-1;
+    x -= px * 1.42860682030941723212E-6;
 
     const double xx = x * x;
 
@@ -105,7 +107,7 @@ static inline double egm1(double x, double px) {
     qx *= xx;
     qx += QX4exp;
 
-    return 2.0d*px / (qx - px);
+    return 2.0 * px / (qx - px);
 }
 
 /// double precision exp function
@@ -115,7 +117,7 @@ static inline double vexp(double initial_x) {
     const int32_t n = px;
 
     x = 1.0 + egm1(x, px);
-    x *= uint642dp((((u_int64_t) n) + 1023) << 52);
+    x *= uint642dp((((uint64_t) n) + 1023) << 52);
 
     if (initial_x > EXP_LIMIT)
         x = inf();
@@ -125,18 +127,14 @@ static inline double vexp(double initial_x) {
     return x;
 }
 
-/// double precision expm1 function
+/// double precision exp function
 static inline double vexpm1(double initial_x) {
     double x = initial_x;
     double px = dpfloor(LOG2E * x + 0.5);
+    const int32_t n = px;
 
-    const int32_t n = ((px > 0.5) || (px >= -0.5)) ? px : 0;
-
-    x = egm1(x, px);
-    const double pow2nm1 = uint642dp((((u_int64_t) n-1) + 1023) << 52);
-    x *= pow2nm1;
-    x += pow2nm1-1;
-    x *= 2;
+    const uint64_t twopnm1 = (((uint64_t)(n - 1)) + 1023) << 52;
+    x = 2 * (uint642dp(twopnm1) * egm1(x, px) + uint642dp(twopnm1)) - 1;
 
     if (initial_x > EXP_LIMIT)
         x = inf();
@@ -146,17 +144,17 @@ static inline double vexpm1(double initial_x) {
     return x;
 }
 
+
 /// double precision exprelr function ()
 static inline double exprelr(double initial_x) {
-    if (1.0+initial_x == 1.0) {
+    if (1.0 + initial_x == 1.0) {
         return 1.0;
     }
 
-    return initial_x/vexpm1(initial_x);
+    return initial_x / vexpm1(initial_x);
 }
 
 static inline float egm1(float x) {
-
     float z = x * PX1expf;
     z += PX2expf;
     z *= x;
@@ -167,7 +165,7 @@ static inline float egm1(float x) {
     z += PX5expf;
     z *= x;
     z += PX6expf;
-    z *= x*x;
+    z *= x * x;
     z += x;
 
     return z;
@@ -194,7 +192,7 @@ static inline float vexp(float initial_x) {
     return z;
 }
 
-/// single precision expm1 function
+/// single precision exp function
 static inline float vexpm1(float initial_x) {
     float x = initial_x;
     float z = spfloor(LOG2EF * x + 0.5f);
@@ -203,25 +201,22 @@ static inline float vexpm1(float initial_x) {
     x -= z * C2F;
     const int32_t n = z;
 
-    z = egm1(x);
-    const double pow2nm1 = uint322sp((n-1 + 0x7f) << 23);
-    z *= pow2nm1;
-    z += pow2nm1-1;
-    z *= 2;
+    const int32_t twopnm1 = ((n - 1) + 0x7f) << 23;
+    x = 2 * (uint322sp(twopnm1) * egm1(x) + uint322sp(twopnm1)) - 1;
 
     if (initial_x > MAXLOGF)
-        z = f_inf();
+        x = f_inf();
     if (initial_x < MINLOGF)
-        z = -1.f;
+        x = -1.0f;
 
-    return z;
+    return x;
 }
 
 /// single precision exprelr function
 static inline float exprelr(float initial_x) {
-    if (1.0+initial_x == 1.0) {
+    if (1.0 + initial_x == 1.0) {
         return 1.0;
     }
 
-    return initial_x/vexpm1(initial_x);
+    return initial_x / vexpm1(initial_x);
 }
