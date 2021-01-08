5e0fee033f9dda926df007075b6d2b6f5714e2a6
George Mitenkov <georgemitenk0v@gmail.com>
2021-01-08 10:58:58 +0300

    Running functions from MOD files via LLVM JIT (#482)
    This commit introduces a functionality to execute functions from MOD file via LLVM jit.
    
    For that, there is now:
    - `JITDriver` class that, given a LLVM IR module, set ups the JIT compiler and is able to look up a function and execute it.
    - `Runner` class that wraps around JIT driver. It helps to initialise JIT with LLVM IR module only once, and then run multiple functions from it. 
    
    To execute functions, `nmodl_llvm_runner` executable is used. It takes a single mod file and a specified entry-point function, and runs it via LLVM code generation pipeline and JIT driver. Only functions with double result types are supported at the moment.
    
    For example, for MOD file `foo.mod`:
    ```
    FUNCTION one() {
        one = 1
    }
    
    FUNCTION bar() {
        bar = one() + exp(1)
    }
    ```
    running `nmodl_llvm_runner -f foo.mod -e bar` gives
    ```
    Result: 3.718282
    ```
    
    Tests for execution of generated  IR have been added as well.
    
    fixes #482
    
    Co-authored-by: Pramod Kumbhar <pramod.s.kumbhar@gmail.com>

---
+++ test/unit/codegen/codegen_llvm_execution.cpp
@@ -0,0 +1,162 @@
+/*************************************************************************
+ * Copyright (C) 2018-2020 Blue Brain Project
+ *
+ * This file is part of NMODL distributed under the terms of the GNU
+ * Lesser General Public License. See top-level LICENSE file for details.
+ *************************************************************************/
+
+#include <catch/catch.hpp>
+#include <regex>
+
+#include "ast/program.hpp"
+#include "codegen/llvm/codegen_llvm_visitor.hpp"
+#include "codegen/llvm/jit_driver.hpp"
+#include "parser/nmodl_driver.hpp"
+#include "visitors/checkparent_visitor.hpp"
+#include "visitors/symtab_visitor.hpp"
+
+using namespace nmodl;
+using namespace runner;
+using namespace visitor;
+using nmodl::parser::NmodlDriver;
+
+static double EPSILON = 1e-15;
+
+//=============================================================================
+// No optimisations
+//=============================================================================
+
+SCENARIO("Arithmetic expression", "[llvm][runner]") {
+    GIVEN("Functions with some arithmetic expressions") {
+        std::string nmodl_text = R"(
+            FUNCTION exponential() {
+                LOCAL i
+                i = 1
+                exponential = exp(i)
+            }
+
+            FUNCTION constant() {
+                constant = 10
+            }
+
+            FUNCTION arithmetic() {
+                LOCAL x, y
+                x = 3
+                y = 7
+                arithmetic = x * y / (x + y)
+            }
+
+            FUNCTION bar() {
+                LOCAL i, j
+                i = 2
+                j = i + 2
+                bar = 2 * 3 + j
+            }
+
+            FUNCTION function_call() {
+                foo()
+                function_call = bar() / constant()
+            }
+
+            PROCEDURE foo() {}
+        )";
+
+
+        NmodlDriver driver;
+        const auto& ast = driver.parse_string(nmodl_text);
+
+        SymtabVisitor().visit_program(*ast);
+        codegen::CodegenLLVMVisitor llvm_visitor(/*mod_filename=*/"unknown",
+                                                 /*output_dir=*/".",
+                                                 /*opt_passes=*/false);
+        llvm_visitor.visit_program(*ast);
+
+        std::unique_ptr<llvm::Module> m = llvm_visitor.get_module();
+        Runner runner(std::move(m));
+
+        THEN("functions are evaluated correctly") {
+            auto exp_result = runner.run<double>("exponential");
+            REQUIRE(fabs(exp_result - 2.718281828459045) < EPSILON);
+
+            auto constant_result = runner.run<double>("constant");
+            REQUIRE(fabs(constant_result - 10.0) < EPSILON);
+
+            auto arithmetic_result = runner.run<double>("arithmetic");
+            REQUIRE(fabs(arithmetic_result - 2.1) < EPSILON);
+
+            auto function_call_result = runner.run<double>("function_call");
+            REQUIRE(fabs(function_call_result - 1.0) < EPSILON);
+        }
+    }
+}
+
+//=============================================================================
+// With optimisations
+//=============================================================================
+
+SCENARIO("Optimised arithmetic expression", "[llvm][runner]") {
+    GIVEN("Functions with some arithmetic expressions") {
+        std::string nmodl_text = R"(
+            FUNCTION exponential() {
+                LOCAL i
+                i = 1
+                exponential = exp(i)
+            }
+
+            FUNCTION constant() {
+                constant = 10 * 2 - 100 / 50 * 5
+            }
+
+            FUNCTION arithmetic() {
+                LOCAL x, y
+                x = 3
+                y = 7
+                arithmetic = x * y / (x + y)
+            }
+
+            FUNCTION bar() {
+                LOCAL i, j
+                i = 2
+                j = i + 2
+                bar = 2 * 3 + j
+            }
+
+            FUNCTION function_call() {
+                foo()
+                function_call = bar() / constant()
+            }
+
+            PROCEDURE foo() {}
+        )";
+
+
+        NmodlDriver driver;
+        const auto& ast = driver.parse_string(nmodl_text);
+
+        SymtabVisitor().visit_program(*ast);
+        codegen::CodegenLLVMVisitor llvm_visitor(/*mod_filename=*/"unknown",
+                                                 /*output_dir=*/".",
+                                                 /*opt_passes=*/true);
+        llvm_visitor.visit_program(*ast);
+
+        std::unique_ptr<llvm::Module> m = llvm_visitor.get_module();
+        Runner runner(std::move(m));
+
+        THEN("optimizations preserve function results") {
+            // Check exponential is turned into a constant.
+            auto exp_result = runner.run<double>("exponential");
+            REQUIRE(fabs(exp_result - 2.718281828459045) < EPSILON);
+
+            // Check constant folding.
+            auto constant_result = runner.run<double>("constant");
+            REQUIRE(fabs(constant_result - 10.0) < EPSILON);
+
+            // Check constant folding.
+            auto arithmetic_result = runner.run<double>("arithmetic");
+            REQUIRE(fabs(arithmetic_result - 2.1) < EPSILON);
+
+            auto function_call_result = runner.run<double>("function_call");
+            REQUIRE(fabs(function_call_result - 1.0) < EPSILON);
+        }
+    }
+}
