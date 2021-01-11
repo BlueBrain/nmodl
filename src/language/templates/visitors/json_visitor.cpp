ea0b8e3b1af68647ee10e3d89d773402d3557653
Pramod S Kumbhar <pramod.s.kumbhar@gmail.com>
2021-01-11 23:13:36 +0100

    Indent json visitor jinja template  - initially template was not indented as code generated    was not looking good  - now all generated code is automatically clang-formatted    so it's less of a concern. Readability is important.

--- src/language/templates/visitors/json_visitor.cpp
+++ src/language/templates/visitors/json_visitor.cpp
@@ -22,33 +22,36 @@ using namespace ast;
 {% for node in nodes %}
 void JSONVisitor::visit_{{ node.class_name|snake_case }}(const {{ node.class_name }}& node) {
     {% if node.has_children() %}
-    printer->push_block(node.get_node_type_name());
-    if (embed_nmodl) {
-        printer->add_block_property("nmodl", to_nmodl(node));
-    }
-    node.visit_children(*this);
-    {% if node.is_data_type_node %}
+        printer->push_block(node.get_node_type_name());
+        if (embed_nmodl) {
+            printer->add_block_property("nmodl", to_nmodl(node));
+        }
+        node.visit_children(*this);
+        {% if node.is_data_type_node %}
             {% if node.is_integer_node %}
-    if(!node.get_macro()) {
-        std::stringstream ss;
-        ss << node.eval();
-        printer->add_node(ss.str());
-    }
+                if(!node.get_macro()) {
+                    std::stringstream ss;
+                    ss << node.eval();
+                    printer->add_node(ss.str());
+                }
             {% else %}
-    std::stringstream ss;
-    ss << node.eval();
-    printer->add_node(ss.str());
+                std::stringstream ss;
+                ss << node.eval();
+                printer->add_node(ss.str());
             {% endif %}
         {% endif %}
-    printer->pop_block();
+
+        printer->pop_block();
+
         {% if node.is_program_node %}
-    if (node.get_parent() == nullptr) {
-        flush();
-    }
+            if (node.get_parent() == nullptr) {
+                flush();
+            }
         {% endif %}
+
     {% else %}
-    (void)node;
-    printer->add_node("{{ node.class_name }}");
+        (void)node;
+        printer->add_node("{{ node.class_name }}");
     {% endif %}
 }
 
