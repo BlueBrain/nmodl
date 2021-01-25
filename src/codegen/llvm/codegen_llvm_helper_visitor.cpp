183acbe86b7131e91298ffc8a27eeb600d784210
George Mitenkov <georgemitenk0v@gmail.com>
2021-01-25 15:59:04 +0300

    Integrating LLVM helper into LLVM visitor (#497)
    * LLVM Helper visitor now can return a vector of `CodegenFunction`s.
    * LLVM Helper visitor has been integrated into LLVM visitor:
       - The type of variables is still double by default, but can also be inferred from `CodegenVarType` node.
       - Procedure's return type changed to int (so that error codes can be returned in the future). 
       - New visitor functions added: for `CodegenReturn`, `CodegenFunction`, `CodegenVarList` and `CodegenVarType`.

--- src/codegen/llvm/codegen_llvm_helper_visitor.cpp
+++ src/codegen/llvm/codegen_llvm_helper_visitor.cpp
@@ -71,6 +71,11 @@ std::shared_ptr<ast::Expression> create_expression(const std::string& code) {
     return std::make_shared<ast::WrappedExpression>(rhs->clone());
 }
 
+CodegenFunctionVector CodegenLLVMHelperVisitor::get_codegen_functions(const ast::Program& node) {
+    const_cast<ast::Program&>(node).accept(*this);
+    return codegen_functions;
+}
+
 /**
  * \brief Add code generation function for FUNCTION or PROCEDURE block
  * @param node AST node representing FUNCTION or PROCEDURE
@@ -98,7 +103,6 @@ std::shared_ptr<ast::Expression> create_expression(const std::string& code) {
  *
  * We perform following transformations so that code generation backends
  * will have minimum logic:
- *  - Add return type
  *  - Add type for the function arguments
  *  - Define variables and return variable
  *  - Add return type (int for PROCEDURE and double for FUNCTION)
