9ca602e2edc1e056670aa68a5e77569e58478dce
George Mitenkov <georgemitenk0v@gmail.com>
2021-02-03 22:45:41 +0300

    Printf support in LLVM IR codegen (#510)
    - Added support for string function arguments. These are
       converted into global `i8` array values.
    - Added support for `printf` function call with variable number 
       of arguments.
    - Refactored function/procedure call argument processing into
       a separate function.
    
    fixes #510

--- src/codegen/llvm/codegen_llvm_visitor.cpp
+++ src/codegen/llvm/codegen_llvm_visitor.cpp
@@ -116,6 +116,11 @@ void CodegenLLVMVisitor::run_llvm_opt_passes() {
 
 void CodegenLLVMVisitor::create_external_method_call(const std::string& name,
                                                      const ast::ExpressionVector& arguments) {
+    if (name == "printf") {
+        create_printf_call(arguments);
+        return;
+    }
+
     std::vector<llvm::Value*> argument_values;
     std::vector<llvm::Type*> argument_types;
     for (const auto& arg: arguments) {
@@ -145,22 +150,37 @@ void CodegenLLVMVisitor::create_function_call(llvm::Function* func,
                                               const std::string& name,
                                               const ast::ExpressionVector& arguments) {
     // Check that function is called with the expected number of arguments.
-    if (arguments.size() != func->arg_size()) {
+    if (!func->isVarArg() && arguments.size() != func->arg_size()) {
         throw std::runtime_error("Error: Incorrect number of arguments passed");
     }
 
-    // Process each argument and add it to a vector to pass to the function call instruction. Note
-    // that type checks are not needed here as NMODL operates on doubles by default.
+    // Pack function call arguments to vector and create a call instruction.
     std::vector<llvm::Value*> argument_values;
-    for (const auto& arg: arguments) {
-        arg->accept(*this);
-        llvm::Value* value = values.back();
-        values.pop_back();
-        argument_values.push_back(value);
-    }
-
+    argument_values.reserve(arguments.size());
+    pack_function_call_arguments(arguments, argument_values);
     llvm::Value* call = builder.CreateCall(func, argument_values);
     values.push_back(call);
+}
+
+void CodegenLLVMVisitor::create_printf_call(const ast::ExpressionVector& arguments) {
+    // First, create printf declaration or insert it if it does not exit.
+    std::string name = "printf";
+    llvm::Function* printf = module->getFunction(name);
+    if (!printf) {
+        llvm::Type* ptr_type = llvm::Type::getInt8PtrTy(*context);
+        llvm::Type* i32_type = llvm::Type::getInt32Ty(*context);
+        llvm::FunctionType* printf_type =
+            llvm::FunctionType::get(i32_type, ptr_type, /*isVarArg=*/true);
+
+        printf =
+            llvm::Function::Create(printf_type, llvm::Function::ExternalLinkage, name, *module);
+    }
+
+    // Create a call instruction.
+    std::vector<llvm::Value*> argument_values;
+    argument_values.reserve(arguments.size());
+    pack_function_call_arguments(arguments, argument_values);
+    builder.CreateCall(printf, argument_values);
 }
 
 void CodegenLLVMVisitor::emit_procedure_or_function_declaration(const ast::CodegenFunction& node) {
@@ -186,6 +206,23 @@ llvm::Value* CodegenLLVMVisitor::lookup(const std::string& name) {
     if (!val)
         throw std::runtime_error("Error: variable " + name + " is not in scope\n");
     return val;
+}
+
+void CodegenLLVMVisitor::pack_function_call_arguments(const ast::ExpressionVector& arguments,
+                                                      std::vector<llvm::Value*>& arg_values) {
+    for (const auto& arg: arguments) {
+        if (arg->is_string()) {
+            // If the argument is a string, create a global i8* variable with it.
+            auto string_arg = std::dynamic_pointer_cast<ast::String>(arg);
+            llvm::Value* str = builder.CreateGlobalStringPtr(string_arg->get_value());
+            arg_values.push_back(str);
+        } else {
+            arg->accept(*this);
+            llvm::Value* value = values.back();
+            values.pop_back();
+            arg_values.push_back(value);
+        }
+    }
 }
 
 llvm::Value* CodegenLLVMVisitor::visit_arithmetic_bin_op(llvm::Value* lhs,
