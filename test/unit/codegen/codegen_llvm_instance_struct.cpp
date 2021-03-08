2d0b6b626550aa255a6baf104d471dd2bfe6a434
Ioannis Magkanaris <iomagkanaris@gmail.com>
2021-03-08 20:01:14 +0100

    Added test for typecasting to the proper struct type (FAILING)

--- test/unit/codegen/codegen_llvm_instance_struct.cpp
+++ test/unit/codegen/codegen_llvm_instance_struct.cpp
@@ -42,7 +42,7 @@ codegen::CodegenInstanceData generate_instance_data(const std::string& text,
     SolveBlockVisitor().visit_program(*ast);
     SymtabVisitor(true).visit_program(*ast);
 
-    codegen::CodegenLLVMVisitor llvm_visitor(/*mod_filename=*/"unknown",
+    codegen::CodegenLLVMVisitor llvm_visitor(/*mod_filename=*/"test",
                                              /*output_dir=*/".",
                                              opt,
                                              use_single_precision);
@@ -57,9 +57,13 @@ codegen::CodegenInstanceData generate_instance_data(const std::string& text,
 template <typename T>
 bool compare_vectors(void* instance_struct_data_ptr, const std::vector<T>& generated_data) {
     std::vector<T> instance_struct_vector;
+    std::cout << "Generated data size: " << generated_data.size() << std::endl;
     instance_struct_vector.assign(static_cast<T*>(instance_struct_data_ptr),
                                   static_cast<T*>(instance_struct_data_ptr) +
                                       generated_data.size());
+    for (auto value: instance_struct_vector) {
+        std::cout << value << std::endl;
+    }
     return instance_struct_vector == generated_data;
 }
 
@@ -98,36 +102,70 @@ SCENARIO("Instance Struct creation", "[visitor][llvm][instance_struct]") {
             }
         )";
 
+        const size_t num_elements = 10;
+        auto instance_data = generate_instance_data(nmodl_text,
+                                                    /*opt=*/false,
+                                                    /*use_single_precision=*/true,
+                                                    num_elements);
+        size_t minf_index = 0;
+        size_t mtau_index = 1;
+        size_t m_index = 2;
+        size_t Dm_index = 3;
+        size_t ena_index = 4;
+        size_t v_unused_index = 5;
+        size_t g_unused_index = 6;
+        size_t ion_ena_index = 7;
+        size_t ion_ena_index_index = 8;
+        size_t voltage_index = 9;
+        size_t node_index_index = 10;
+        size_t t_index = 11;
+        size_t dt_index = 12;
+        size_t celsius_index = 13;
+        size_t secondorder_index = 14;
+        size_t node_count_index = 15;
+
         THEN("instance struct elements are properly initialized") {
-            const size_t num_elements = 100;
-            size_t initial_value = 0;
-            auto instance_data = generate_instance_data(nmodl_text,
-                                                        /*opt=*/false,
-                                                        /*use_single_precision=*/true,
-                                                        num_elements);
-
-            size_t minf_index = 0;
-            size_t mtau_index = 1;
-            size_t m_index = 2;
-            size_t Dm_index = 3;
-            size_t ena_index = 4;
-            size_t v_unused_index = 5;
-            size_t g_unused_index = 6;
-            size_t ion_ena_index = 7;
-            size_t ion_ena_index_index = 8;
-            size_t voltage_index = 9;
-            size_t node_index_index = 10;
-            size_t t_index = 11;
-            size_t dt_index = 12;
-            size_t celsius_index = 13;
-            size_t secondorder_index = 14;
-            size_t node_count_index = 15;
-
-            // Check the float values are loaded correctly and added
-            REQUIRE(*static_cast<double*>(instance_data.members[celsius_index]) ==
-                    default_celsius_value);
+            // Check if the various instance struct fields are properly initialized
             REQUIRE(compare_vectors(instance_data.members[minf_index],
                                     generate_double_data(minf_index, num_elements)));
+            REQUIRE(compare_vectors(instance_data.members[ena_index],
+                                    generate_double_data(ena_index, num_elements)));
+            REQUIRE(compare_vectors(instance_data.members[ion_ena_index],
+                                    generate_double_data(ion_ena_index, num_elements)));
+            REQUIRE(compare_vectors(instance_data.members[node_index_index],
+                                    generate_int_data(node_index_index, num_elements)));
+            REQUIRE(*static_cast<double*>(instance_data.members[t_index]) ==
+                    default_nthread_t_value);
+            REQUIRE(*static_cast<int*>(instance_data.members[node_count_index]) == num_elements);
+        }
+
+        THEN("instance struct is properly initialized") {
+            // Hard code test__instance_var__type struct
+            struct test__instance_var__type {
+                double* minf;
+                double* mtau;
+                double* m;
+                double* Dm;
+                double* ena;
+                double* v_unused;
+                double* g_unused;
+                double* ion_ena;
+                int* ion_ena_index;
+                double* voltage;
+                int* node_index;
+                double t;
+                double dt;
+                double celsius;
+                int secondorder;
+                int node_count;
+            };
+            // Test if test__instance_var__type struct is properly initialized
+            test__instance_var__type* test_instance_struct = static_cast<test__instance_var__type*>(
+                instance_data.base_ptr);
+            REQUIRE(compare_vectors(test_instance_struct->minf,
+                                    generate_double_data(minf_index, num_elements)));
+            REQUIRE(test_instance_struct->t == default_nthread_t_value);
+            REQUIRE(test_instance_struct->celsius == default_celsius_value);
         }
     }
 }
