6c7f0a96322a96afcac08aaae98b3da70610e9c7
George Mitenkov <georgemitenk0v@gmail.com>
2021-04-17 09:51:19 +0300

    Benchmarking LLVM code generation (#583)
    Introduced the benchmarking for LLVM code generation pipeline.
    For that, new options have been added:
    
    ```
    benchmark
      LLVM benchmark option
      Options:
        --run                                                               Run LLVM benchmark (false)
        --instance-size INT                                       Instance struct size (10000)
        --repeat INT                                                    Number of experiments for benchmarking (100)
        --backend TEXT:{avx2, default, sse2}         Target's backend (default)
    ```
    
    The JIT runner has also been modified to extract the target
    information correctly, and disable available CPU features for
    benchmarking a specific backend.
    
    Example:
    ```
    $ nmodl hh.mod llvm --ir --vector-width 1 benchmark --run --instance-size 100 --repeat 2 --backend default
    
    Created LLVM IR module from NMODL AST in 0.006765817
    
    Benchmarking kernel 'nrn_state_hh'
    Experiment 0: compute time = 0.013977749
    Experiment 1: compute time = 0.004847989
    Average compute time = 0.0058550929
    ```
    
    Co-authored-by: Pramod Kumbhar <pramod.s.kumbhar@gmail.com>

---
+++ src/codegen/llvm/llvm_benchmark.hpp
@@ -0,0 +1,85 @@
+/*************************************************************************
+ * Copyright (C) 2018-2021 Blue Brain Project
+ *
+ * This file is part of NMODL distributed under the terms of the GNU
+ * Lesser General Public License. See top-level LICENSE file for details.
+ *************************************************************************/
+
+#pragma once
+
+#include <string>
+
+#include "codegen/llvm/codegen_llvm_visitor.hpp"
+
+
+namespace nmodl {
+namespace benchmark {
+
+/// A struct to hold LLVM visitor information.
+struct LLVMBuildInfo {
+    int vector_width;
+    bool opt_passes;
+    bool use_single_precision;
+};
+
+/**
+ * \class LLVMBenchmark
+ * \brief A wrapper to execute MOD file kernels via LLVM IR backend, and
+ * benchmark compile-time and runtime.
+ */
+class LLVMBenchmark {
+  private:
+    std::string mod_filename;
+
+    std::string output_dir;
+
+    int num_experiments;
+
+    int instance_size;
+
+    std::string backend;
+
+    LLVMBuildInfo llvm_build_info;
+
+    std::shared_ptr<std::ostream> log_stream;
+
+    /// Disable the specified feature.
+    void disable(const std::string& feature, std::vector<std::string>& host_features);
+
+    /// Visits the AST to construct the LLVM IR module.
+    void generate_llvm(codegen::CodegenLLVMVisitor& visitor,
+                       const std::shared_ptr<ast::Program>& node);
+
+    /// Get the host CPU features in the format:
+    ///   +feature,+feature,-feature,+feature,...
+    /// where `+` indicates that the feature is enabled.
+    std::vector<std::string> get_cpu_features();
+
+    /// Runs the main body of the benchmark, executing the compute kernels.
+    void run_benchmark(codegen::CodegenLLVMVisitor& visitor,
+                       const std::shared_ptr<ast::Program>& node);
+
+    /// Sets the log output stream (file or console).
+    void set_log_output();
+
+  public:
+    LLVMBenchmark(const std::string& mod_filename,
+                  const std::string& output_dir,
+                  LLVMBuildInfo info,
+                  int num_experiments,
+                  int instance_size,
+                  const std::string& backend)
+        : mod_filename(mod_filename)
+        , output_dir(output_dir)
+        , num_experiments(num_experiments)
+        , instance_size(instance_size)
+        , backend(backend)
+        , llvm_build_info(info) {}
+
+    /// Runs the benchmark.
+    void benchmark(const std::shared_ptr<ast::Program>& node);
+};
+
+
+}  // namespace benchmark
+}  // namespace nmodl
