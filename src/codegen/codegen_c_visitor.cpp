neuronsimulator/nrn

commit 520f60e4bcd5ef3ffbbdf5b281f63aa41cea2d99

Remove support of AoS layout (#565)
Consider that we always use SoA.

NMODL currently has support for AoS layout. This was added for compatibility and testing with MOD2C and CoreNEURON.
We remove AoS support due to following reasons:

In practice, we never use AoS layout for CPU vectorisation or GPUs
This part remains untested because of the same reason
Simplify the code

What have been done:
Remove CLI option in nmodl for layout
Remove all logic in src/codegen where we we are using LayoutType::aos.

Fix #563

src/codegen/codegen_c_visitor.cpp
@@ -1921,7 +1921,7 @@ std::string CodegenCVisitor::process_verbatim_text(std::string text) {
             name = "&" + name;
         }
         if (token == "_STRIDE") {
-            name = (layout == LayoutType::soa) ? "pnodecount+id" : "1";
+            name = "pnodecount+id";
         }
         result += name;
     }
@@ -2024,18 +2024,6 @@ void CodegenCVisitor::print_nmodl_constants() {
 }
 
 
-void CodegenCVisitor::print_memory_layout_getter() {
-    printer->add_newline(2);
-    printer->add_line("static inline int get_memory_layout() {");
-    if (layout == LayoutType::aos) {
-        printer->add_line("    return 1;  //aos");
-    } else {
-        printer->add_line("    return 0;  //soa");
-    }
-    printer->add_line("}");
-}
-
-
 void CodegenCVisitor::print_first_pointer_var_index_getter() {
     printer->add_newline(2);
     print_device_method_annotation();
@@ -2090,14 +2078,6 @@ void CodegenCVisitor::print_memb_list_getter() {
     printer->add_line("    }");
     printer->add_line("    return nt->_ml_list[get_mech_type()];");
     printer->add_line("}");
-}
-
-
-void CodegenCVisitor::print_post_channel_iteration_common_code() {
-    if (layout == LayoutType::aos) {
-        printer->add_line("data = ml->data + id*{};"_format(float_variables_size()));
-        printer->add_line("indexes = ml->pdata + id*{};"_format(int_variables_size()));
-    }
 }
 
 
@@ -2182,18 +2162,14 @@ std::string CodegenCVisitor::float_variable_name(const SymbolType& symbol,
     // clang-format off
     if (symbol->is_array()) {
         if (use_instance) {
-            auto stride = (layout == LayoutType::soa) ? dimension : num_float;
-            return "(inst->{}+id*{})"_format(name, stride);
+            return "(inst->{}+id*{})"_format(name, dimension);
         }
-        auto stride = (layout == LayoutType::soa) ? "{}*pnodecount+id*{}"_format(position, dimension) : "{}"_format(position);
-        return "(data+{})"_format(stride);
+        return "(data + {}*pnodecount + id*{})"_format(position, dimension);
     }
     if (use_instance) {
-        auto stride = (layout == LayoutType::soa) ? "id" : "id*{}"_format(num_float);
-        return "inst->{}[{}]"_format(name, stride);
+        return "inst->{}[id]"_format(name);
     }
-    auto stride = (layout == LayoutType::soa) ? "{}*pnodecount+id"_format(position) : "{}"_format(position);
-    return "data[{}]"_format(stride);
+    return "data[{}*pnodecount + id]"_format(position);
     // clang-format on
 }
 
@@ -2203,29 +2179,24 @@ std::string CodegenCVisitor::int_variable_name(const IndexVariableInfo& symbol,
                                                bool use_instance) const {
     auto position = position_of_int_var(name);
     auto num_int = int_variables_size();
-    std::string offset;
     // clang-format off
     if (symbol.is_index) {
-        offset = std::to_string(position);
         if (use_instance) {
-            return "inst->{}[{}]"_format(name, offset);
+            return "inst->{}[{}]"_format(name, position);
         }
-        return "indexes[{}]"_format(offset);
+        return "indexes[{}]"_format(position);
     }
     if (symbol.is_integer) {
         if (use_instance) {
-            offset = (layout == LayoutType::soa) ? "{}*pnodecount+id"_format(position) : "id*{}+{}"_format(num_int, position);
-            return "inst->{}[{}]"_format(name, offset);
+            return "inst->{}[{}*pnodecount+id]"_format(name, position);
         }
-        offset = (layout == LayoutType::soa) ? "{}*pnodecount+id"_format(position) : "id";
-        return "indexes[{}]"_format(offset);
+        return "indexes[{}*pnodecount+id]"_format(position);
     }
-    offset = (layout == LayoutType::soa) ? "{}*pnodecount+id"_format(position) : "{}"_format(position);
     if (use_instance) {
-        return "inst->{}[indexes[{}]]"_format(name, offset);
+        return "inst->{}[indexes[{}*pnodecount + id]]"_format(name, position);
     }
     auto data = symbol.is_vdata ? "_vdata" : "_data";
-    return "nt->{}[indexes[{}]]"_format(data, offset);
+    return "nt->{}[indexes[{}]]"_format(data, position);
     // clang-format on
 }
 
@@ -2622,8 +2593,6 @@ void CodegenCVisitor::print_global_variables_for_hoc() {
  *  - If nrn_get_mechtype is < -1 means that mechanism is not used in the
  *    context of neuron execution and hence could be ignored in coreneuron
  *    execution.
- *  - Each mechanism could have different layout and hence we register the
- *    layout with the simulator. In practice all mechanisms have same layout.
  *  - Ions are internally defined and their types can be queried similar to
  *    other mechanisms.
  *  - hoc_register_var may not be needed in the context of coreneuron
@@ -2646,7 +2615,7 @@ void CodegenCVisitor::print_mechanism_register() {
     printer->add_line("}");
 
     printer->add_newline();
-    printer->add_line("_nrn_layout_reg(mech_type, get_memory_layout());");
+    printer->add_line("_nrn_layout_reg(mech_type, 0);");  // 0 for SoA
 
     // register mechanism
     auto args = register_mechanism_arguments();
@@ -2999,7 +2968,7 @@ void CodegenCVisitor::print_global_variable_setup() {
                 value = *value_ptr;
             }
             /// use %g to be same as nocmodl in neuron
-            printer->add_line("{} = {};"_format(name, "{:g}"_format(value)));
+            printer->add_line("{} = {:g};"_format(name, value));
         }
     }
 
@@ -3012,7 +2981,7 @@ void CodegenCVisitor::print_global_variable_setup() {
             value = *value_ptr;
         }
         /// use %g to be same as nocmodl in neuron
-        printer->add_line("{} = {};"_format(name, "{:g}"_format(value)));
+        printer->add_line("{} = {:g};"_format(name, value));
     }
 
     if (info.table_count > 0) {
@@ -3139,10 +3108,8 @@ void CodegenCVisitor::print_instance_variable_setup() {
     }
 
     std::string stride;
-    if (layout == LayoutType::soa) {
-        printer->add_line("int pnodecount = ml->_nodecount_padded;");
-        stride = "*pnodecount";
-    }
+    printer->add_line("int pnodecount = ml->_nodecount_padded;");
+    stride = "*pnodecount";
 
     printer->add_line("Datum* indexes = ml->pdata;");
 
@@ -3317,8 +3284,6 @@ void CodegenCVisitor::print_nrn_init(bool skip_init_check) {
     print_channel_iteration_tiling_block_begin(BlockType::Initial);
     print_channel_iteration_block_begin(BlockType::Initial);
 
-    print_post_channel_iteration_common_code();
-
     if (info.net_receive_node != nullptr) {
         printer->add_line("{} = -1e20;"_format(get_variable_name("tsave")));
     }
@@ -3411,7 +3376,6 @@ void CodegenCVisitor::print_watch_check() {
     print_global_function_common_code(BlockType::Watch);
     print_channel_iteration_tiling_block_begin(BlockType::Watch);
     print_channel_iteration_block_begin(BlockType::Watch);
-    print_post_channel_iteration_common_code();
 
     if (info.is_voltage_used_by_watch_statements()) {
         printer->add_line("int node_id = node_index[id];");
@@ -3798,9 +3762,7 @@ void CodegenCVisitor::visit_for_netcon(const ast::ForNetcon& node) {
         })->index;
     const auto num_int = int_variables_size();
 
-    std::string offset = (layout == LayoutType::soa) ? "{}*pnodecount + id"_format(index)
-                                                     : "{} + id*{}"_format(index, num_int);
-    printer->add_text("const size_t offset = {};"_format(offset));
+    printer->add_text("const size_t offset = {}*pnodecount + id;"_format(index));
     printer->add_newline();
     printer->add_line(
         "const size_t for_netcon_start = nt->_fornetcon_perm_indices[indexes[offset]];");
@@ -3925,7 +3887,7 @@ void CodegenCVisitor::print_derivimplicit_kernel(Block* block) {
     auto list_num = info.derivimplicit_list_num;
     auto block_name = block->get_node_name();
     auto primes_size = info.primes_size;
-    auto stride = (layout == LayoutType::aos) ? "" : "*pnodecount+id";
+    auto stride = "*pnodecount+id";
 
     printer->add_newline(2);
 
@@ -4037,7 +3999,6 @@ void CodegenCVisitor::print_nrn_state() {
     print_global_function_common_code(BlockType::State);
     print_channel_iteration_tiling_block_begin(BlockType::State);
     print_channel_iteration_block_begin(BlockType::State);
-    print_post_channel_iteration_common_code();
 
     printer->add_line("int node_id = node_index[id];");
     printer->add_line("double v = voltage[node_id];");
@@ -4252,7 +4213,6 @@ void CodegenCVisitor::print_nrn_cur() {
     print_global_function_common_code(BlockType::Equation);
     print_channel_iteration_tiling_block_begin(BlockType::Equation);
     print_channel_iteration_block_begin(BlockType::Equation);
-    print_post_channel_iteration_common_code();
     print_nrn_cur_kernel(*info.breakpoint_node);
     print_nrn_cur_matrix_shadow_update();
     if (!nrn_cur_reduction_loop_required()) {
@@ -4300,7 +4260,6 @@ void CodegenCVisitor::print_namespace_end() {
 
 void CodegenCVisitor::print_common_getters() {
     print_first_pointer_var_index_getter();
-    print_memory_layout_getter();
     print_net_receive_arg_size_getter();
     print_thread_getters();
     print_num_variable_getter();

src/codegen/codegen_acc_visitor.hpp
@@ -98,17 +98,15 @@ class CodegenAccVisitor: public CodegenCVisitor {
   public:
     CodegenAccVisitor(const std::string& mod_file,
                       const std::string& output_dir,
-                      LayoutType layout,
                       const std::string& float_type,
                       bool optimize_ionvar_copies)
-        : CodegenCVisitor(mod_file, output_dir, layout, float_type, optimize_ionvar_copies) {}
+        : CodegenCVisitor(mod_file, output_dir, float_type, optimize_ionvar_copies) {}
 
     CodegenAccVisitor(const std::string& mod_file,
                       std::ostream& stream,
-                      LayoutType layout,
                       const std::string& float_type,
                       bool optimize_ionvar_copies)
-        : CodegenCVisitor(mod_file, stream, layout, float_type, optimize_ionvar_copies) {}
+        : CodegenCVisitor(mod_file, stream, float_type, optimize_ionvar_copies) {}
 };
 
 /** @} */  // end of codegen_backends

src/codegen/codegen_c_visitor.hpp
@@ -133,20 +133,6 @@ struct IndexVariableInfo {
 
 
 /**
- * \enum LayoutType
- * \brief Represents memory layout to use for code generation
- *
- */
-enum class LayoutType {
-    /// array of structure
-    aos,
-
-    /// structure of array
-    soa
-};
-
-
-/**
  * \class ShadowUseStatement
  * \brief Represents ion write statement during code generation
  *
@@ -269,11 +255,6 @@ class CodegenCVisitor: public visitor::ConstAstVisitor {
      * Data type of floating point variables
      */
     std::string float_type = codegen::naming::DEFAULT_FLOAT_TYPE;
-
-    /**
-     * Memory layout for code generation
-     */
-    LayoutType layout;
 
     /**
      * All ast information for code generation
@@ -1122,13 +1103,6 @@ class CodegenCVisitor: public visitor::ConstAstVisitor {
 
 
     /**
-     * Print the getter method for memory layout
-     *
-     */
-    void print_memory_layout_getter();
-
-
-    /**
      * Print the getter method for index position of first pointer variable
      *
      */
@@ -1312,12 +1286,6 @@ class CodegenCVisitor: public visitor::ConstAstVisitor {
      * Print backend specific channel instance iteration block end
      */
     virtual void print_channel_iteration_block_end();
-
-
-    /**
-     * Print common code post channel instance iteration
-     */
-    void print_post_channel_iteration_common_code();
 
 
     /**
@@ -1644,7 +1612,6 @@ class CodegenCVisitor: public visitor::ConstAstVisitor {
 
     CodegenCVisitor(const std::string& mod_filename,
                     const std::string& output_dir,
-                    LayoutType layout,
                     const std::string& float_type,
                     const bool optimize_ionvar_copies,
                     const std::string& extension,
@@ -1653,13 +1620,11 @@ class CodegenCVisitor: public visitor::ConstAstVisitor {
         , wrapper_printer(new CodePrinter(output_dir + "/" + mod_filename + wrapper_ext))
         , printer(target_printer)
         , mod_filename(mod_filename)
-        , layout(layout)
         , float_type(float_type)
         , optimize_ionvar_copies(optimize_ionvar_copies) {}
 
     CodegenCVisitor(const std::string& mod_filename,
                     std::ostream& stream,
-                    LayoutType layout,
                     const std::string& float_type,
                     const bool optimize_ionvar_copies,
                     const std::string& extension,
@@ -1668,7 +1633,6 @@ class CodegenCVisitor: public visitor::ConstAstVisitor {
         , wrapper_printer(new CodePrinter(stream))
         , printer(target_printer)
         , mod_filename(mod_filename)
-        , layout(layout)
         , float_type(float_type)
         , optimize_ionvar_copies(optimize_ionvar_copies) {}
 
@@ -1687,21 +1651,18 @@ class CodegenCVisitor: public visitor::ConstAstVisitor {
      * \param mod_filename The name of the model for which code should be generated.
      *                     It is used for constructing an output filename.
      * \param output_dir   The directory where target C file should be generated.
-     * \param layout       The memory layout to be used for data structure generation.
      * \param float_type   The float type to use in the generated code. The string will be used
      *                     as-is in the target code. This defaults to \c double.
      * \param extension    The file extension to use. This defaults to \c .cpp .
      */
     CodegenCVisitor(const std::string& mod_filename,
                     const std::string& output_dir,
-                    LayoutType layout,
                     std::string float_type,
                     const bool optimize_ionvar_copies,
                     const std::string& extension = ".cpp")
         : target_printer(new CodePrinter(output_dir + "/" + mod_filename + extension))
         , printer(target_printer)
         , mod_filename(mod_filename)
-        , layout(layout)
         , float_type(std::move(float_type))
         , optimize_ionvar_copies(optimize_ionvar_copies) {}
 
@@ -1718,19 +1679,16 @@ class CodegenCVisitor: public visitor::ConstAstVisitor {
      * \param mod_filename The name of the model for which code should be generated.
      *                     It is used for constructing an output filename.
      * \param stream       The output stream onto which to write the generated code
-     * \param layout       The memory layout to be used for data structure generation.
      * \param float_type   The float type to use in the generated code. The string will be used
      *                     as-is in the target code. This defaults to \c double.
      */
     CodegenCVisitor(const std::string& mod_filename,
                     std::ostream& stream,
-                    LayoutType layout,
                     const std::string& float_type,
                     const bool optimize_ionvar_copies)
         : target_printer(new CodePrinter(stream))
         , printer(target_printer)
         , mod_filename(mod_filename)
-        , layout(layout)
         , float_type(float_type)
         , optimize_ionvar_copies(optimize_ionvar_copies) {}
 
@@ -1747,21 +1705,18 @@ class CodegenCVisitor: public visitor::ConstAstVisitor {
      *
      * \param mod_filename   The name of the model for which code should be generated.
      *                       It is used for constructing an output filename.
-     * \param layout         The memory layout to be used for data structure generation.
      * \param float_type     The float type to use in the generated code. The string will be used
      *                       as-is in the target code. This defaults to \c double.
      * \param target_printer A printer defined outside this visitor to be used for the code
      *                       generation
      */
     CodegenCVisitor(std::string mod_filename,
-                    LayoutType layout,
                     std::string float_type,
                     const bool optimize_ionvar_copies,
                     std::shared_ptr<CodePrinter>& target_printer)
         : target_printer(target_printer)
         , printer(target_printer)
         , mod_filename(mod_filename)
-        , layout(layout)
         , float_type(float_type)
         , optimize_ionvar_copies(optimize_ionvar_copies) {}
 

src/codegen/codegen_cuda_visitor.hpp
@@ -106,18 +106,15 @@ class CodegenCudaVisitor: public CodegenCVisitor {
   public:
     CodegenCudaVisitor(const std::string& mod_file,
                        const std::string& output_dir,
-                       LayoutType layout,
                        const std::string& float_type,
                        const bool optimize_ionvar_copies)
-        : CodegenCVisitor(mod_file, output_dir, layout, float_type, optimize_ionvar_copies, ".cu") {
-    }
+        : CodegenCVisitor(mod_file, output_dir, float_type, optimize_ionvar_copies, ".cu") {}
 
     CodegenCudaVisitor(const std::string& mod_file,
                        std::ostream& stream,
-                       LayoutType layout,
                        const std::string& float_type,
                        const bool optimize_ionvar_copies)
-        : CodegenCVisitor(mod_file, stream, layout, float_type, optimize_ionvar_copies) {}
+        : CodegenCVisitor(mod_file, stream, float_type, optimize_ionvar_copies) {}
 };
 
 /** @} */  // end of codegen_backends

src/codegen/codegen_ispc_visitor.hpp
@@ -220,32 +220,18 @@ class CodegenIspcVisitor: public CodegenCVisitor {
   public:
     CodegenIspcVisitor(const std::string& mod_file,
                        const std::string& output_dir,
-                       LayoutType layout,
                        const std::string& float_type,
                        const bool optimize_ionvar_copies)
-        : CodegenCVisitor(mod_file,
-                          output_dir,
-                          layout,
-                          float_type,
-                          optimize_ionvar_copies,
-                          ".ispc",
-                          ".cpp")
-        , fallback_codegen(mod_file, layout, float_type, optimize_ionvar_copies, wrapper_printer) {}
+        : CodegenCVisitor(mod_file, output_dir, float_type, optimize_ionvar_copies, ".ispc", ".cpp")
+        , fallback_codegen(mod_file, float_type, optimize_ionvar_copies, wrapper_printer) {}
 
 
     CodegenIspcVisitor(const std::string& mod_file,
                        std::ostream& stream,
-                       LayoutType layout,
                        const std::string& float_type,
                        const bool optimize_ionvar_copies)
-        : CodegenCVisitor(mod_file,
-                          stream,
-                          layout,
-                          float_type,
-                          optimize_ionvar_copies,
-                          ".ispc",
-                          ".cpp")
-        , fallback_codegen(mod_file, layout, float_type, optimize_ionvar_copies, wrapper_printer) {}
+        : CodegenCVisitor(mod_file, stream, float_type, optimize_ionvar_copies, ".ispc", ".cpp")
+        , fallback_codegen(mod_file, float_type, optimize_ionvar_copies, wrapper_printer) {}
 
     void visit_function_call(const ast::FunctionCall& node) override;
     void visit_var_name(const ast::VarName& node) override;

src/codegen/codegen_omp_visitor.hpp
@@ -72,17 +72,15 @@ class CodegenOmpVisitor: public CodegenCVisitor {
   public:
     CodegenOmpVisitor(std::string mod_file,
                       std::string output_dir,
-                      LayoutType layout,
                       std::string float_type,
                       const bool optimize_ionvar_copies)
-        : CodegenCVisitor(mod_file, output_dir, layout, float_type, optimize_ionvar_copies) {}
+        : CodegenCVisitor(mod_file, output_dir, float_type, optimize_ionvar_copies) {}
 
     CodegenOmpVisitor(std::string mod_file,
                       std::stringstream& stream,
-                      LayoutType layout,
                       std::string float_type,
                       const bool optimize_ionvar_copies)
-        : CodegenCVisitor(mod_file, stream, layout, float_type, optimize_ionvar_copies) {}
+        : CodegenCVisitor(mod_file, stream, float_type, optimize_ionvar_copies) {}
 };
 
 /** @} */  // end of codegen_backends

src/nmodl/main.cpp
@@ -152,9 +152,6 @@ int main(int argc, const char* argv[]) {
     /// true if symbol table should be printed
     bool show_symtab(false);
 
-    /// memory layout for code generation
-    std::string layout("soa");
-
     /// floating point data type
     std::string data_type("double");
 
@@ -247,12 +244,8 @@ int main(int argc, const char* argv[]) {
         "Write symbol table to stdout ({})"_format(show_symtab))->ignore_case();
 
     auto codegen_opt = app.add_subcommand("codegen", "Code generation options")->ignore_case();
-    codegen_opt->add_option("--layout",
-        layout,
-        "Memory layout for code generation",
-        true)->ignore_case()->check(CLI::IsMember({"aos", "soa"}));
     codegen_opt->add_option("--datatype",
-        layout,
+        data_type,
         "Data type for floating point variables",
         true)->ignore_case()->check(CLI::IsMember({"float", "double"}));
     codegen_opt->add_flag("--force",
@@ -507,41 +500,48 @@ int main(int argc, const char* argv[]) {
         }
 
         {
-            auto mem_layout = layout == "aos" ? codegen::LayoutType::aos : codegen::LayoutType::soa;
-
-
             if (ispc_backend) {
                 logger->info("Running ISPC backend code generator");
-                CodegenIspcVisitor visitor(
-                    modfile, output_dir, mem_layout, data_type, optimize_ionvar_copies_codegen);
+                CodegenIspcVisitor visitor(modfile,
+                                           output_dir,
+                                           data_type,
+                                           optimize_ionvar_copies_codegen);
                 visitor.visit_program(*ast);
             }
 
             else if (oacc_backend) {
                 logger->info("Running OpenACC backend code generator");
-                CodegenAccVisitor visitor(
-                    modfile, output_dir, mem_layout, data_type, optimize_ionvar_copies_codegen);
+                CodegenAccVisitor visitor(modfile,
+                                          output_dir,
+                                          data_type,
+                                          optimize_ionvar_copies_codegen);
                 visitor.visit_program(*ast);
             }
 
             else if (omp_backend) {
                 logger->info("Running OpenMP backend code generator");
-                CodegenOmpVisitor visitor(
-                    modfile, output_dir, mem_layout, data_type, optimize_ionvar_copies_codegen);
+                CodegenOmpVisitor visitor(modfile,
+                                          output_dir,
+                                          data_type,
+                                          optimize_ionvar_copies_codegen);
                 visitor.visit_program(*ast);
             }
 
             else if (c_backend) {
                 logger->info("Running C backend code generator");
-                CodegenCVisitor visitor(
-                    modfile, output_dir, mem_layout, data_type, optimize_ionvar_copies_codegen);
+                CodegenCVisitor visitor(modfile,
+                                        output_dir,
+                                        data_type,
+                                        optimize_ionvar_copies_codegen);
                 visitor.visit_program(*ast);
             }
 
             if (cuda_backend) {
                 logger->info("Running CUDA backend code generator");
-                CodegenCudaVisitor visitor(
-                    modfile, output_dir, mem_layout, data_type, optimize_ionvar_copies_codegen);
+                CodegenCudaVisitor visitor(modfile,
+                                           output_dir,
+                                           data_type,
+                                           optimize_ionvar_copies_codegen);
                 visitor.visit_program(*ast);
             }
         }

test/unit/codegen/codegen_ispc.cpp
@@ -69,8 +69,8 @@ class IspcCodegenTestHelper {
 
         /// initialize CodegenIspcVisitor
         std::ostream oss(&strbuf);
-        codegen_ispc_visitor = std::make_shared<CodegenIspcVisitor>(
-            "unit_test", oss, codegen::LayoutType::soa, "double", false);
+        codegen_ispc_visitor =
+            std::make_shared<CodegenIspcVisitor>("unit_test", oss, "double", false);
         codegen_ispc_visitor->setup(*ast);
     }
 

BlueBrain/nmodl

commit 4321fc6ced614fe8257bf366d164289b6af0ee55

Fix expand of indexes
* This bug has been introduced by 520f60e4bcd5e
* By simplifying code we forget to expand resolution of indexes

src/codegen/codegen_c_visitor.cpp
@@ -2196,7 +2196,7 @@ std::string CodegenCVisitor::int_variable_name(const IndexVariableInfo& symbol,
         return "inst->{}[indexes[{}*pnodecount + id]]"_format(name, position);
     }
     auto data = symbol.is_vdata ? "_vdata" : "_data";
-    return "nt->{}[indexes[{}]]"_format(data, position);
+    return "nt->{}[indexes[{}*pnodecount + id]]"_format(data, position);
     // clang-format on
 }
 