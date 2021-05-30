92142f28976c6b7e7d319e20e3e617a12ac322c6
Pramod Kumbhar <pramod.s.kumbhar@gmail.com>
2021-05-30 14:58:13 +0200

    fix test for AARCH64 or X86_64

--- test/unit/codegen/codegen_llvm_ir.cpp
+++ test/unit/codegen/codegen_llvm_ir.cpp
@@ -1383,14 +1383,19 @@ SCENARIO("Vector library calls", "[visitor][llvm][vector_lib]") {
             REQUIRE(std::regex_search(accelerate_library_module_str, m, accelerate_exp_call));
             REQUIRE(!std::regex_search(accelerate_library_module_str, m, fexp_call));
 
-            // Check correct replacement of @llvm.exp.v4f32 into @_ZGVnN4v_expf when using SLEEF.
+            // Check correct replacement of @llvm.exp.v2f64 into @_ZGV?N?v_exp when using SLEEF.
             std::string sleef_library_module_str = run_llvm_visitor(nmodl_text,
                                                                     /*opt=*/false,
-                                                                    /*use_single_precision=*/true,
-                                                                    /*vector_width=*/4,
+                                                                    /*use_single_precision=*/false,
+                                                                    /*vector_width=*/2,
                                                                     /*vec_lib=*/"SLEEF");
-            std::regex sleef_exp_decl(R"(declare <4 x float> @_ZGVnN4v_expf\(<4 x float>\))");
-            std::regex sleef_exp_call(R"(call <4 x float> @_ZGVnN4v_expf\(<4 x float> .*\))");
+#if defined(__arm64__) || defined(__aarch64__)
+            std::regex sleef_exp_decl(R"(declare <2 x double> @_ZGVnN2v_exp\(<2 x double>\))");
+            std::regex sleef_exp_call(R"(call <2 x double> @_ZGVnN2v_exp\(<2 x double> .*\))");
+#else
+            std::regex sleef_exp_decl(R"(declare <2 x double> @_ZGVbN2v_exp\(<2 x double>\))");
+            std::regex sleef_exp_call(R"(call <2 x double> @_ZGVbN2v_exp\(<2 x double> .*\))");
+#endif
             REQUIRE(std::regex_search(sleef_library_module_str, m, sleef_exp_decl));
             REQUIRE(std::regex_search(sleef_library_module_str, m, sleef_exp_call));
             REQUIRE(!std::regex_search(sleef_library_module_str, m, fexp_call));
