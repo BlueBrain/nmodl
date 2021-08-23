b0f90ca5f706180d5b81facd221c053885910c37
Ioannis Magkanaris <iomagkanaris@gmail.com>
2021-08-23 20:22:57 +0300

    Added test with multiple simple cases

--- test/unit/visitor/defuse_analyze.cpp
+++ test/unit/visitor/defuse_analyze.cpp
@@ -395,6 +395,56 @@ SCENARIO("Perform DefUse analysis on NMODL constructs") {
         }
     }
 
+    GIVEN("Simple check of local and global variables") {
+        std::string nmodl_text = R"(
+            NEURON {
+                GLOBAL x
+            }
+
+            DERIVATIVE states {
+                LOCAL a, b
+                a = 1
+                IF (x == 1) {
+                    LOCAL c
+                    c = 1
+                }
+            }
+        )";
+
+        std::string expected_text_x =
+            R"({"DerivativeBlock":[{"CONDITIONAL_BLOCK":[{"IF":[{"name":"U"}]}]}]})";
+        std::string expected_text_a =
+            R"({"DerivativeBlock":[{"name":"LD"},{"CONDITIONAL_BLOCK":[{"name":"IF"}]}]})";
+        std::string expected_text_b =
+            R"({"DerivativeBlock":[{"CONDITIONAL_BLOCK":[{"name":"IF"}]}]})";
+        std::string expected_text_c =
+            R"({"DerivativeBlock":[{"CONDITIONAL_BLOCK":[{"IF":[{"name":"LD"}]}]}]})";
+
+        THEN(
+            "local and global variables are correctly analyzed") {
+            std::string input = reindent_text(nmodl_text);
+            auto chains_x = run_defuse_visitor(input, "x");
+            // Global variable "x" should be U as it's only used in the IF-ELSE statement
+            REQUIRE(chains_x[0].to_string() == expected_text_x);
+            REQUIRE(chains_x[0].eval() == DUState::U);
+
+            auto chains_a = run_defuse_visitor(input, "a");
+            // Local variable "a" should be LD as it's local and defined
+            REQUIRE(chains_a[0].to_string() == expected_text_a);
+            REQUIRE(chains_a[0].eval() == DUState::LD);
+
+            auto chains_b = run_defuse_visitor(input, "b");
+            // Local variable "b" should be NONE as it's not used
+            REQUIRE(chains_b[0].to_string() == expected_text_b);
+            REQUIRE(chains_b[0].eval() == DUState::NONE);
+
+            auto chains_c = run_defuse_visitor(input, "c");
+            // Local variable "c" should be CD as it's conditionally defined
+            REQUIRE(chains_c[0].to_string() == expected_text_c);
+            REQUIRE(chains_c[0].eval() == DUState::CD);
+        }
+    }
+
     GIVEN("global variable definition in if-else block") {
         std::string nmodl_text = R"(
             NEURON {
