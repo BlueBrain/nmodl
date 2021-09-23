908b80ffcb7fe93724518da24d9cc2f02f7a6659
Ioannis Magkanaris <iomagkanaris@gmail.com>
2021-09-23 20:52:23 +0200

    Fix for fi.mod and ampanmda.mod from olfactory

--- src/codegen/codegen_acc_visitor.cpp
+++ src/codegen/codegen_acc_visitor.cpp
@@ -303,7 +303,10 @@ void CodegenAccVisitor::print_device_stream_wait() const {
 
 void CodegenAccVisitor::print_net_send_buf_count_update_to_host() const {
     print_device_stream_wait();
+    printer->start_block("if (nsb) ");
     printer->add_line("#pragma acc update self(nsb->_cnt) if(nt->compute_gpu)");
+    printer->add_line("update_net_send_buffer_on_host(nt, nsb);");
+    printer->end_block(1);
 }
 
 
