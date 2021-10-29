240ac8dd5e8c2c12a30ee20d4fd929ab32505e38
Olli Lupton <oliver.lupton@epfl.ch>
2021-10-29 13:35:14 +0200

    pybind11: remove explicit dependencies.
    Use type names instead of passing instances of pybind11::class_, this
    makes the different instantiations in pyast.cpp more independent of one
    another.

--- src/language/templates/pybind/pyast.cpp
+++ src/language/templates/pybind/pyast.cpp
@@ -28,12 +28,8 @@
 
 
 // clang-format off
-{% macro var(node) -%}
-{{ node.class_name | snake_case }}_
-{%- endmacro -%}
-
 {% macro args(children) %}
-{% for c in children %} {{ c.get_shared_typename() }} {%- if not loop.last %}, {% endif %} {% endfor %}
+{% for c in children %}{{ c.get_shared_typename() }}{%- if not loop.last %}, {% endif %}{% endfor %}
 {%- endmacro -%}
 // clang-format on
 
@@ -227,65 +223,62 @@ void init_ast_module(py::module& m) {
     {% endfor %}
 
     {% for node in nodes %}
-    py::class_<{{ node.class_name }}, std::shared_ptr<{{ node.class_name }}>> {{ var(node) }}(m_ast, "{{ node.class_name }}", {{ node.base_class | snake_case }}_);
-    {{ var(node) }}.doc() = "{{ node.brief }}";
-    {% if node.children %}
-    {{ var(node) }}.def(py::init<{{ args(node.children) }}>());
-    {% endif %}
-    {% if node.is_program_node or node.is_ptr_excluded_node %}
-    {{ var(node) }}.def(py::init<>());
-    {% endif %}
-    // clang-format on
+    {
+        py::class_<{{ node.class_name }}, {{node.base_class}}, std::shared_ptr<{{ node.class_name }}>> tmp{m_ast, "{{ node.class_name }}"};
+        tmp.doc() = "{{ node.brief }}";
+        {% if node.children %}
+        tmp.def(py::init<{{ args(node.children) }}>());
+        {% endif %}
+        {% if node.is_program_node or node.is_ptr_excluded_node %}
+        tmp.def(py::init<>());
+        {% endif %}
+        // clang-format on
 
-    {{var(node)}}
-        .def("__repr__", []({{node.class_name}} & n) {
-            std::stringstream ss;
-            JSONVisitor v(ss);
-            v.compact_json(true);
-            n.accept(v);
-            v.flush();
-            return ss.str();
-        });
+        tmp.def("__repr__", []({{node.class_name}} & n) {
+                std::stringstream ss;
+                JSONVisitor v(ss);
+                v.compact_json(true);
+                n.accept(v);
+                v.flush();
+                return ss.str();
+            });
+        tmp.def("__str__", []({{node.class_name}} & n) {
+                std::stringstream ss;
+                NmodlPrintVisitor v(ss);
+                n.accept(v);
+                return ss.str();
+            });
 
-    {{var(node)}}
-        .def("__str__", []({{node.class_name}} & n) {
-            std::stringstream ss;
-            NmodlPrintVisitor v(ss);
-            n.accept(v);
-            return ss.str();
-        });
+        // clang-format off
+        {% for member in node.public_members() %}
+        tmp.def_readwrite("{{ member[1] }}", &{{ node.class_name }}::{{ member[1] }});
+        {% endfor %}
 
-    // clang-format off
-    {% for member in node.public_members() %}
-    {{ var(node) }}.def_readwrite("{{ member[1] }}", &{{ node.class_name }}::{{ member[1] }});
+        {% for member in node.properties() %}
+        {% if member[2] == True %}
+        tmp.def_property("{{ member[1] }}", &{{ node.class_name }}::get_{{ member[1] }}, &{{ node.class_name }}::set_{{ member[1] }});
+        {% else %}
+        tmp.def_property("{{ member[1] }}", &{{ node.class_name }}::get_{{ member[1] }}, static_cast<void ({{ node.class_name }}::*)(const {{ member[0] }}&)>(&{{ node.class_name }}::set_{{ member[1] }}));
+        {% endif %}
+        {% endfor %}
+
+        tmp.def("visit_children", static_cast<void ({{ node.class_name }}::*)(visitor::Visitor&)>(&{{ node.class_name }}::visit_children), docstring::visit_children_method)
+           .def("accept", static_cast<void ({{ node.class_name }}::*)(visitor::Visitor&)>(&{{ node.class_name }}::accept), docstring::accept_method)
+           .def("accept", static_cast<void ({{ node.class_name }}::*)(visitor::ConstVisitor&) const>(&{{ node.class_name }}::accept), docstring::accept_method)
+           .def("clone", &{{ node.class_name }}::clone, docstring::clone_method)
+           .def("get_node_type", &{{ node.class_name }}::get_node_type, docstring::get_node_type_method)
+           .def("get_node_type_name", &{{ node.class_name }}::get_node_type_name, docstring::get_node_type_name_method)
+        {% if node.nmodl_name %}
+           .def("get_nmodl_name", &{{ node.class_name }}::get_nmodl_name, docstring::get_nmodl_name_method)
+        {% endif %}
+        {% if node.is_data_type_node %}
+           .def("eval", &{{ node.class_name }}::eval, docstring::eval_method)
+        {% endif %}
+           .def("is_{{ node.class_name | snake_case }}", &{{ node.class_name }}::is_{{ node.class_name | snake_case }}, "Check if node is of type ast.{{ node.class_name}}");
+
+        // clang-format on
+    }
     {% endfor %}
-
-    {% for member in node.properties() %}
-    {% if member[2] == True %}
-        {{ var(node) }}.def_property("{{ member[1] }}", &{{ node.class_name }}::get_{{ member[1] }},
-            &{{ node.class_name }}::set_{{ member[1] }});
-    {% else %}
-        {{ var(node) }}.def_property("{{ member[1] }}", &{{ node.class_name }}::get_{{ member[1] }},
-                static_cast<void ({{ node.class_name }}::*)(const {{ member[0] }}&)>(&{{ node.class_name }}::set_{{ member[1] }}));
-    {% endif %}
-    {% endfor %}
-
-    {{ var(node) }}.def("visit_children", static_cast<void ({{ node.class_name }}::*)(visitor::Visitor&)>(&{{ node.class_name }}::visit_children), docstring::visit_children_method)
-    .def("accept", static_cast<void ({{ node.class_name }}::*)(visitor::Visitor&)>(&{{ node.class_name }}::accept), docstring::accept_method)
-    .def("accept", static_cast<void ({{ node.class_name }}::*)(visitor::ConstVisitor&) const>(&{{ node.class_name }}::accept), docstring::accept_method)
-    .def("clone", &{{ node.class_name }}::clone, docstring::clone_method)
-    .def("get_node_type", &{{ node.class_name }}::get_node_type, docstring::get_node_type_method)
-    .def("get_node_type_name", &{{ node.class_name }}::get_node_type_name, docstring::get_node_type_name_method)
-    {% if node.nmodl_name %}
-    .def("get_nmodl_name", &{{ node.class_name }}::get_nmodl_name, docstring::get_nmodl_name_method)
-    {% endif %}
-    {% if node.is_data_type_node %}
-    .def("eval", &{{ node.class_name }}::eval, docstring::eval_method)
-    {% endif %}
-    .def("is_{{ node.class_name | snake_case }}", &{{ node.class_name }}::is_{{ node.class_name | snake_case }}, "Check if node is of type ast.{{ node.class_name}}");
-
-    {% endfor %}
-    // clang-format on
 }
 
 #pragma clang diagnostic pop
