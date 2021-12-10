98f53b8591e58d5141563cd30cc81b2d398b9b82
Pramod Kumbhar <kumbhar@login1.ascent.olcf.ornl.gov>
2021-12-09 19:25:35 -0500

    LLVM OpenMP offload: simd can not be nested within target parallel loop
    ```
    /ccsopen/proj/gen170/neuron/nersc-gpu-hackathon-dec-2021/users/kumbhar/CoreNeuron_XL/coreneuron/sim/treeset_core.cpp:101:9: error: OpenMP constructs may not be
          nested inside a simd region
            nrn_pragma_omp(atomic update)
    ```

--- src/codegen/codegen_acc_visitor.cpp
+++ src/codegen/codegen_acc_visitor.cpp
@@ -54,7 +54,7 @@ void CodegenAccVisitor::print_channel_iteration_block_parallel_hint(BlockType ty
         "nrn_pragma_acc(parallel loop {} async(nt->stream_id) if(nt->compute_gpu))"_format(
             present_clause.str()));
     printer->add_line(
-        "nrn_pragma_omp(target teams distribute parallel for simd is_device_ptr(inst) "
+        "nrn_pragma_omp(target teams distribute parallel for is_device_ptr(inst) "
         "if(nt->compute_gpu))");
 }
 
