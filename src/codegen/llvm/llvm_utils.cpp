b6463d328af632dbacb6ef0e04a519f89d9fcd87
George Mitenkov <georgemitenk0v@gmail.com>
2022-03-24 10:20:52 +0100

    Added NVPTX-specific optimization passes for PTX  generation

--- src/codegen/llvm/llvm_utils.cpp
+++ src/codegen/llvm/llvm_utils.cpp
@@ -13,8 +13,10 @@
 #include "llvm/IR/Verifier.h"
 #include "llvm/InitializePasses.h"
 #include "llvm/Support/FileSystem.h"
+#include "llvm/Support/TargetSelect.h"
 #include "llvm/Support/ToolOutputFile.h"
 #include "llvm/Target/TargetMachine.h"
+#include "llvm/Transforms/IPO.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 
 namespace nmodl {
@@ -60,6 +62,71 @@ static void run_optimisation_passes(llvm::Module& module,
 /****************************************************************************************/
 /*                             Optimisation utils                                       */
 /****************************************************************************************/
+
+void initialise_nvptx_passes() {
+    // Register targets.
+    LLVMInitializeNVPTXTarget();
+    LLVMInitializeNVPTXTargetMC();
+    LLVMInitializeNVPTXTargetInfo();
+    LLVMInitializeNVPTXAsmPrinter();
+
+    // Initialize passes.
+    initialise_optimisation_passes();
+}
+
+void optimise_module_for_nvptx(llvm::Module& module, int opt_level, std::string& target_asm) {
+    // CUDA target machine we generating code for.
+    std::unique_ptr<llvm::TargetMachine> tm;
+
+    // Hardcode target infromation for now. Change if necessary.
+    llvm::Triple triple("nvptx64-nvidia-cuda");
+    std::string subtarget = "sm_20";
+    std::string features = "+ptx60";
+
+    // Find the specified target in registry.
+    std::string error_msg;
+    auto* target = llvm::TargetRegistry::lookupTarget("", triple, error_msg);
+    if (!target)
+        throw std::runtime_error("Error: " + error_msg + "\n");
+
+    tm.reset(target->createTargetMachine(triple.str(), subtarget, features, {}, {}));
+    if (!tm)
+        throw std::runtime_error("Error: creating target machine failed! Aborting.");
+
+    // Set data layout and target triple information for the module. Note
+    // that  we may want to have a more elaborate layout than the one
+    // created by `createDataLayout()`.
+    module.setDataLayout(tm->createDataLayout());
+    module.setTargetTriple("nvptx64-nvidia-cuda");
+
+    llvm::legacy::FunctionPassManager func_pm(&module);
+    llvm::legacy::PassManager module_pm;
+    llvm::PassManagerBuilder pm_builder;
+    pm_builder.OptLevel = opt_level;
+    pm_builder.SizeLevel = 0;
+    pm_builder.Inliner = llvm::createFunctionInliningPass();
+
+    // Do not vectorize!
+    pm_builder.LoopVectorize = false;
+
+    // Adjusting pass manager adds target-specific IR transformations, e.g.
+    // inferring address spaces.
+    tm->adjustPassManager(pm_builder);
+    pm_builder.populateFunctionPassManager(func_pm);
+    pm_builder.populateModulePassManager(module_pm);
+
+    // This runs target-indepependent optimizations.
+    run_optimisation_passes(module, func_pm, module_pm);
+
+    // Now, we want to run target-specific (e.g. NVPTX) passes. In LLVM, this
+    // is done via `addPassesToEmitFile`.
+    llvm::raw_string_ostream stream(target_asm);
+    llvm::buffer_ostream pstream(stream);
+    llvm::legacy::PassManager codegen_pm;
+
+    tm->addPassesToEmitFile(codegen_pm, pstream, nullptr, llvm::CGFT_AssemblyFile);
+    codegen_pm.run(module);
+}
 
 void initialise_optimisation_passes() {
     auto& registry = *llvm::PassRegistry::getPassRegistry();
--- src/codegen/llvm/llvm_utils.hpp
+++ src/codegen/llvm/llvm_utils.hpp
@@ -16,6 +16,12 @@ namespace utils {
 /// Initialises some LLVM optimisation passes.
 void initialise_optimisation_passes();
 
+/// Initialises NVPTX-specific optimisation passes.
+void initialise_nvptx_passes();
+
+/// Optimises the given LLVM IR module for NVPTX targets.
+void optimise_module_for_nvptx(llvm::Module& module, int opt_level, std::string& target_asm);
+
 /// Optimises the given LLVM IR module.
 void optimise_module(llvm::Module& module, int opt_level, llvm::TargetMachine* tm = nullptr);
 
