84a85d4d702de2a57834e9ada6d15c88e0310b54
George Mitenkov <georgemitenk0v@gmail.com>
2022-03-28 13:08:09 +0200

    [LLVM][GPU] NVPTX specific passes for code generation (#833)
    * Added NVPTX-specific optimization passes for PTX generation
    * Added tests
    * Added CLI options to select 32- or 64-bit targets and target compute architecture (e.g. sm_70)
    
    Co-authored-by: Ioannis Magkanaris <iomagkanaris@gmail.com>

--- src/codegen/llvm/llvm_utils.cpp
+++ src/codegen/llvm/llvm_utils.cpp
@@ -13,8 +13,10 @@
 #include "llvm/IR/Verifier.h"
 #include "llvm/InitializePasses.h"
 #include "llvm/Support/FileSystem.h"
+#include "llvm/Support/TargetSelect.h"
 #include "llvm/Support/ToolOutputFile.h"
 #include "llvm/Target/TargetMachine.h"
+#include "llvm/Transforms/IPO.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 
 namespace nmodl {
@@ -60,6 +62,85 @@ static void run_optimisation_passes(llvm::Module& module,
 /****************************************************************************************/
 /*                             Optimisation utils                                       */
 /****************************************************************************************/
+
+void initialise_nvptx_passes() {
+    // Register targets.
+    LLVMInitializeNVPTXTarget();
+    LLVMInitializeNVPTXTargetMC();
+    LLVMInitializeNVPTXTargetInfo();
+    LLVMInitializeNVPTXAsmPrinter();
+
+    // Initialize passes.
+    initialise_optimisation_passes();
+}
+
+void optimise_module_for_nvptx(codegen::Platform& platform,
+                               llvm::Module& module,
+                               int opt_level,
+                               std::string& target_asm) {
+    // CUDA target machine we generating code for.
+    std::unique_ptr<llvm::TargetMachine> tm;
+    std::string platform_name = platform.get_name();
+
+    // Target and layout information.
+    static const std::map<std::string, std::string> triple_str = {
+            {"nvptx", "nvptx-nvidia-cuda"},
+            {"nvptx64", "nvptx64-nvidia-cuda"}};
+    static const std::map<std::string, std::string> data_layout_str = {
+            {"nvptx", "e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32"
+                      "-i64:64:64-f32:32:32-f64:64:64-v16:16:16-v32:32:32"
+                      "-v64:64:64-v128:128:128-n16:32:64"},
+            {"nvptx64", "e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32"
+                        "-i64:64:64-f32:32:32-f64:64:64-v16:16:16-v32:32:32"
+                        "-v64:64:64-v128:128:128-n16:32:64"}};
+
+    // Set data layout and target triple information for the module.
+    auto triple = triple_str.at(platform_name);
+    module.setDataLayout(data_layout_str.at(platform_name));
+    module.setTargetTriple(triple);
+
+    std::string subtarget = platform.get_subtarget_name();
+    std::string features = "+ptx70";
+
+    // Find the specified target in registry.
+    std::string error_msg;
+    auto* target = llvm::TargetRegistry::lookupTarget(triple, error_msg);
+    if (!target)
+        throw std::runtime_error("Error: " + error_msg + "\n");
+
+    tm.reset(target->createTargetMachine(triple, subtarget, features, {}, {}));
+    if (!tm)
+        throw std::runtime_error("Error: creating target machine failed! Aborting.");
+
+    // Create pass managers.
+    llvm::legacy::FunctionPassManager func_pm(&module);
+    llvm::legacy::PassManager module_pm;
+    llvm::PassManagerBuilder pm_builder;
+    pm_builder.OptLevel = opt_level;
+    pm_builder.SizeLevel = 0;
+    pm_builder.Inliner = llvm::createFunctionInliningPass();
+
+    // Do not vectorize!
+    pm_builder.LoopVectorize = false;
+
+    // Adjusting pass manager adds target-specific IR transformations, e.g.
+    // inferring address spaces.
+    tm->adjustPassManager(pm_builder);
+    pm_builder.populateFunctionPassManager(func_pm);
+    pm_builder.populateModulePassManager(module_pm);
+
+    // This runs target-indepependent optimizations.
+    run_optimisation_passes(module, func_pm, module_pm);
+
+    // Now, we want to run target-specific (e.g. NVPTX) passes. In LLVM, this
+    // is done via `addPassesToEmitFile`.
+    llvm::raw_string_ostream stream(target_asm);
+    llvm::buffer_ostream pstream(stream);
+    llvm::legacy::PassManager codegen_pm;
+
+    tm->addPassesToEmitFile(codegen_pm, pstream, nullptr, llvm::CGFT_AssemblyFile);
+    codegen_pm.run(module);
+}
 
 void initialise_optimisation_passes() {
     auto& registry = *llvm::PassRegistry::getPassRegistry();
