4c86ae4b8ec368290cb102a33a200a5eb7203e60
Omar Awile <omar.awile@epfl.ch>
2022-04-27 11:52:55 +0200

    Add a PyJIT integration test

---
+++ test/benchmark/benchmark.py
@@ -0,0 +1,25 @@
+import sys
+
+import nmodl.dsl as nmodl
+from nmodl import ast, visitor
+
+def main():
+    driver = nmodl.NmodlDriver()
+    lookup_visitor = visitor.AstLookupVisitor()
+
+    cfg = nmodl.CodeGenConfig()
+    cfg.llvm_vector_width = 4
+    cfg.llvm_opt_level_ir = 2
+    fname = sys.argv[1]
+    with open(fname) as f:
+        hh = f.read()
+        modast = driver.parse_string(hh)
+        modname = lookup_visitor.lookup(modast, ast.AstNodeType.SUFFIX)[0].get_node_name()
+        jit = nmodl.Jit(cfg)
+
+        res = jit.run(modast, modname, 1000, 1000)
+        print(res)
+
+
+if __name__ == "__main__":
+    main()
