a8a44ed9f834e9ecc5aaacffb304cbf1dce1eb30
Ioannis Magkanaris <ioannis.magkanaris@epfl.ch>
2022-05-03 16:20:48 +0300

    Added arg parsing in benchmark script and added expsyn.mod test

--- test/benchmark/benchmark.py
+++ test/benchmark/benchmark.py
@@ -1,19 +1,33 @@
+import argparse
 import sys
 import os
 
 import nmodl.dsl as nmodl
 from nmodl import ast, visitor
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Benchmark test script for NMODL.')
+    parser.add_argument('--gpu', action='store_true', default=False,
+                        help='Enable GPU JIT execution')
+    parser.add_argument('--vec', type=int, default=1,
+                        help='Vector width for CPU execution')
+    parser.add_argument('--file', type=str,
+                        help='NMODL file to benchmark')
+    args, _ = parser.parse_known_args()
+    return args
+
 def main():
+    args = parse_arguments()
+
     driver = nmodl.NmodlDriver()
     lookup_visitor = visitor.AstLookupVisitor()
 
     cfg = nmodl.CodeGenConfig()
-    cfg.llvm_vector_width = 4
+    cfg.llvm_vector_width = args.vec
     cfg.llvm_opt_level_ir = 2
     cfg.nmodl_ast = True
-    fname = sys.argv[1]
-    if len(sys.argv) > 2:  # GPU enabled
+    fname = args.file
+    if args.gpu:  # GPU enabled
         cfg.llvm_math_library = "libdevice"
         cfg.llvm_gpu_name = "nvptx64"
         cfg.llvm_gpu_target_architecture = "sm_70"
