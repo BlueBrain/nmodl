23e337e9d373a907228278fa1a2ca2af6ccd2b29
Ioannis Magkanaris <ioannis.magkanaris@epfl.ch>
2022-05-05 13:32:35 +0300

    Small function renaming, added comments with explanation of CUDA JIT workflow and made clang-format happier

--- src/pybind/pynmodl.cpp
+++ src/pybind/pynmodl.cpp
@@ -154,8 +154,7 @@ class JitDriver {
         platform = nmodl::codegen::Platform(
             pid, name, cfg.llvm_math_library, cfg.llvm_float_type, cfg.llvm_vector_width);
         if (platform.is_gpu() && !platform.is_CUDA_gpu()) {
-            throw std::runtime_error(
-                "Benchmarking is only supported on CUDA GPUs at the moment");
+            throw std::runtime_error("Benchmarking is only supported on CUDA GPUs at the moment");
         }
 #ifndef NMODL_LLVM_CUDA_BACKEND
         if (platform.is_CUDA_gpu()) {
@@ -193,8 +192,7 @@ class JitDriver {
         cg_driver.prepare_mod(node, modname);
         nmodl::codegen::CodegenLLVMVisitor visitor(modname, cfg.output_dir, platform, 0);
         visitor.visit_program(*node);
-        const GPUExecutionParameters gpu_execution_parameters{cuda_grid_dim_x,
-                                                              cuda_block_dim_x};
+        const GPUExecutionParameters gpu_execution_parameters{cuda_grid_dim_x, cuda_block_dim_x};
         nmodl::benchmark::LLVMBenchmark benchmark(visitor,
                                                   modname,
                                                   cfg.output_dir,
@@ -281,7 +279,8 @@ PYBIND11_MODULE(_nmodl, m_nmodl) {
         .def_readwrite("llvm_fast_math_flags", &nmodl::codegen::CodeGenConfig::llvm_fast_math_flags)
         .def_readwrite("llvm_cpu_name", &nmodl::codegen::CodeGenConfig::llvm_cpu_name)
         .def_readwrite("llvm_gpu_name", &nmodl::codegen::CodeGenConfig::llvm_gpu_name)
-        .def_readwrite("llvm_gpu_target_architecture", &nmodl::codegen::CodeGenConfig::llvm_gpu_target_architecture)
+        .def_readwrite("llvm_gpu_target_architecture",
+                       &nmodl::codegen::CodeGenConfig::llvm_gpu_target_architecture)
         .def_readwrite("llvm_vector_width", &nmodl::codegen::CodeGenConfig::llvm_vector_width)
         .def_readwrite("llvm_opt_level_codegen",
                        &nmodl::codegen::CodeGenConfig::llvm_opt_level_codegen)
