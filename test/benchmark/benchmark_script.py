b961331b3bc13103bba5753cdc2929038b119af7
Ioannis Magkanaris <iomagkanaris@gmail.com>
2022-05-20 18:21:44 +0200

    Work towards running JIT vie python

--- test/benchmark/benchmark_script.py
+++ test/benchmark/benchmark_script.py
@@ -1,6 +1,10 @@
 import argparse
 
-class CompilersConfig():
+import nmodl.dsl as nmodl
+from nmodl import ast, visitor
+
+
+class CompilersConfig:
     svml_lib = ""
     intel_exe = ""
     sleef_lib = ""
@@ -9,7 +13,18 @@ class CompilersConfig():
     gcc_exe = ""
     libdevice_lib = ""
     nmodl_exe = ""
-    def __init__(self, svml_lib, intel_exe, sleef_lib, clang_exe, llc_exe, gcc_exe, libdevice_lib, nmodl_exe):
+
+    def __init__(
+        self,
+        svml_lib,
+        intel_exe,
+        sleef_lib,
+        clang_exe,
+        llc_exe,
+        gcc_exe,
+        libdevice_lib,
+        nmodl_exe,
+    ):
         self.svml_lib = svml_lib
         self.intel_exe = intel_exe
         self.sleef_lib = sleef_lib
@@ -19,13 +34,14 @@ class CompilersConfig():
         self.libdevice_lib = libdevice_lib
         self.nmodl_exe = nmodl_exe
 
+
 class BenchmarkConfig:
     mod_files = ""
     architectures = ""
     compilers = ""
     math_libraries = ["SVML", "SLEEF"]
     fast_math = [False, True]
-    llvm_fast_math_flags = "--fmf nnan contract afn"
+    llvm_fast_math_flags = "nnan contract afn"
     external_kernel = False
     instances = 100000000
     experiments = 5
@@ -33,7 +49,7 @@ class BenchmarkConfig:
     output_directory = "benchmark_output"
     ext_lib_name = "libextkernel.so"
     compiler_flags = {}
-    compiler_flags["intel"]["skylake_avx512"] = [
+    compiler_flags["intel"]["skylake-avx512"] = [
         "-O2 -march=skylake-avx512 -mtune=skylake -prec-div -fimf-use-svml",
         "-O2 -march=skylake-avx512 -mtune=skylake -prec-div -fimf-use-svml -fopenmp",
     ]
@@ -41,7 +57,7 @@ class BenchmarkConfig:
         "-O2 -march=broadwell -mtune=broadwell -prec-div -fimf-use-svml"
     ]
     compiler_flags["intel"]["nehalem"] = ["-O2 -msse2 -prec-div -fimf-use-svml"]
-    compiler_flags["clang"]["skylake_avx512"] = [
+    compiler_flags["clang"]["skylake-avx512"] = [
         "-O3 -march=skylake-avx512 -mtune=skylake -ffast-math",
         "-O3 -march=skylake-avx512 -mtune=skylake -ffast-math -fopenmp",
         "-O3 -march=skylake-avx512 -mtune=skylake -ffast-math -fveclib=SVML",
@@ -55,7 +71,7 @@ class BenchmarkConfig:
         "-O3 -march=nehalem -mtune=nehalem -ffast-math -fopenmp",
         "-O3 -march=nehalem -mtune=nehalem -ffast-math -fopenmp -fveclib=SVML",
     ]
-    compiler_flags["gcc"]["skylake_avx512"] = [
+    compiler_flags["gcc"]["skylake-avx512"] = [
         "-O3 -march=skylake-avx512 -mtune=skylake -ffast-math -ftree-vectorize",
         "-O3 -march=skylake-avx512 -mtune=skylake -ffast-math -ftree-vectorize -fopenmp",
         "-O3 -march=skylake-avx512 -mtune=skylake -ffast-math -ftree-vectorize -mveclibabi=svml",
@@ -110,62 +126,145 @@ class Benchmark:
         self.compiler_config = compiler_config
         self.benchmark_config = benchmark_config
 
+    def translate_mod_file_to_cpp(self, mod_file):
+        """Translate mod file to cpp file that can be
+        compiled by the compilers specified and then
+        executed by NMODL
+        """
+        pass
+
+    def compile_external_library(self, cpp_file, compiler, architecture, flags):
+        """Compile cpp_file to an external shared library
+        that has the state and current kernels and can be
+        then loaded by NMODL to execute these kernels
+        """
+        pass
+
     def run_external_kernel(self):
         """Runs all external kernel related benchmarks"""
         pass
 
-    def run_JIT_kernels(self):
+    def run_JIT_kernels(
+        self,
+        modast,
+        modname,
+        architecture,
+        gpu_target_architecture,
+        fast_math_flags,
+        math_lib,
+        instances,
+        experiments,
+    ):
         """Runs NMODL JIT kernels"""
-        pass
+        cfg = nmodl.CodeGenConfig()
+        cfg.llvm_ir = True
+        cfg.llvm_opt_level_ir = 3
+        cfg.llvm_math_library = math_lib
+        cfg.llvm_fast_math_flags = fast_math_flags
+        if architecture != "nvptx64":
+            cfg.llvm_cpu_name = architecture
+        else:
+            cfg.llvm_gpu_name = "nvptx64"
+            cfg.llvm_gpu_target_architecture = gpu_target_architecture
+        if architecture == "skylake-avx512":
+            cfg.llvm_vector_width = 8
+        elif architecture == "broadwell":
+            cfg.llvm_vector_width = 4
+        elif architecture == "nehalem":
+            cfg.llvm_vector_width = 2
+        else:
+            cfg.llvm_vector_width = 1
+        cfg.llvm_opt_level_codegen = 3
+        if math_lib == "SVML":
+            cfg.shared_lib_paths = self.compiler_config.svml_lib
+        elif math_lib == "SLEEF":
+            cfg.shared_lib_paths = self.compiler_config.sleef_lib
+        cfg.output_dir = self.benchmark_config.output_directory
+        jit = nmodl.Jit(cfg)
+        res = jit.run(modast, modname, 1000, 1000)
+        return res
 
     def run_benchmark(self):
-        for modfile in self.config.mod_files:
+        for modfile in self.benchmark_config.mod_files:
             if modfile not in self.results:
                 self.results[modfile] = {}
-            for architecture in self.config.architectures:
-                if architecture not in self.results[modfile]:
-                    self.results[modfile][architecture] = {}
-                if self.config.external_kernel:
-                    for compiler in self.config.compilers:
-                        if compiler not in self.results[modfile][architecture]:
-                            self.results[modfile][architecture][compiler] = {}
-                        for flags in self.config.compiler_flags[compiler][architecture]:
-                            # Translate mod file to .cpp to be compiled by the certain compiler
-                            # Compile the .cpp file to a shared library
-                            # Run NMODL JIT with external shared library
+            # Make number of instances smalle for hh kernel due to it's already large memory footprint
+            if [modfile == "hh.mod"]:
+                kernel_instance_size = self.benchmark_config.instances / 5
+            else:
+                kernel_instance_size = self.benchmark_config.instances
+
+            driver = nmodl.NmodlDriver()
+            lookup_visitor = visitor.AstLookupVisitor()
+
+            with open(modfile) as f:
+                hh = f.read()
+                modast = driver.parse_string(hh)
+                modname = lookup_visitor.lookup(modast, ast.AstNodeType.SUFFIX)[
+                    0
+                ].get_node_name()
+
+                for architecture in self.benchmark_config.architectures:
+                    if architecture not in self.results[modfile]:
+                        self.results[modfile][architecture] = {}
+                    if self.benchmark_config.external_kernel:
+                        for compiler in self.benchmark_config.compilers:
+                            if compiler not in self.results[modfile][architecture]:
+                                self.results[modfile][architecture][compiler] = {}
+                            for flags in self.benchmark_config.compiler_flags[compiler][
+                                architecture
+                            ]:
+                                # Translate mod file to .cpp to be compiled by the certain compiler
+                                # Compile the .cpp file to a shared library
+                                # Run NMODL JIT with external shared library
+                                print(
+                                    "self.results[modfile][architecture][compiler][flags] = jit.run(modast, modname, self.config.instances, self.config.experiments, external_lib)"
+                                )
+                                if compiler == "clang":
+                                    for (
+                                        math_lib
+                                    ) in self.benchmark_config.math_libraries:
+                                        # Generate LLVM IR from NMODL JIT
+                                        # sed the nrn_state_hh name to _Z16nrn_state_hh_extPv to match the external kernel signature name of the external shared lib
+                                        # compile LLVM IR using clang and the compiler flags of the architecture used and generate shared library
+                                        # Run NMODL JIT with external shared library
+                                        print(
+                                            'self.results[modfile][architecture][compiler][flags+"jit"] = jit.run(modast, modname, self.config.instances, self.config.experiments, external_lib)'
+                                        )
+                    for fast_math in self.benchmark_config.fast_math:
+                        if fast_math:
+                            fast_math_flags = self.llvm_fast_math_flags
+                            fast_math_name = "nnancontractafn"
+                        else:
+                            fast_math_flags = ""
+                            fast_math_name = "nonfastmath"
+                        if architecture != "nvptx64":
+                            for math_lib in self.benchmark_config.math_libraries:
+                                # Run NMODL JIT on CPU
+                                print(
+                                    'self.results[modfile][architecture]["nmodl_jit"][math_lib+fast_math_name] = jit.run(modast, modname, self.config.instances, self.config.experiments)'
+                                )
+                                self.results[modfile][architecture]["nmodl_jit"][
+                                    math_lib + "_" + fast_math_name
+                                ] = self.run_JIT_kernels(
+                                    modname,
+                                    modast,
+                                    architecture,
+                                    "",
+                                    fast_math_flags,
+                                    math_lib,
+                                    kernel_instance_size,
+                                    self.benchmark_config.experiments,
+                                )
+                        else:
+                            # Run NMODL JIT on GPU
                             print(
-                                "self.results[modfile][architecture][compiler][flags] = jit.run(modast, modname, self.config.instances, self.config.experiments, external_lib)"
+                                'self.results[modfile][architecture]["nmodl_jit_cuda"]["libdevice"+fast_math_name] = jit.run(modast, modname, self.config.instances, self.config.experiments)'
                             )
-                            if compiler == "clang":
-                                for math_lib in self.config.math_libraries:
-                                    # Generate LLVM IR from NMODL JIT
-                                    # sed the nrn_state_hh name to _Z16nrn_state_hh_extPv to match the external kernel signature name of the external shared lib
-                                    # compile LLVM IR using clang and the compiler flags of the architecture used and generate shared library
-                                    # Run NMODL JIT with external shared library
-                                    print(
-                                        'self.results[modfile][architecture][compiler][flags+"jit"] = jit.run(modast, modname, self.config.instances, self.config.experiments, external_lib)'
-                                    )
-                for fast_math in self.config.fast_math:
-                    if fast_math:
-                        fast_math_flags = self.llvm_fast_math_flags
-                        fast_math_name = "nnancontractafn"
-                    else:
-                        fast_math_flags = ""
-                        fast_math_name = "nonfastmath"
-                    if architecture != "nvptx64":
-                        for math_lib in self.config.math_libraries:
-                            # Run NMODL JIT on CPU
-                            print(
-                                'self.results[modfile][architecture]["nmodl_jit"][math_lib+fast_math_name] = jit.run(modast, modname, self.config.instances, self.config.experiments)'
-                            )
-                    else:
-                        # Run NMODL JIT on CPU
-                        print(
-                            'self.results[modfile][architecture]["nmodl_jit_cuda"]["libdevice"+fast_math_name] = jit.run(modast, modname, self.config.instances, self.config.experiments)'
-                        )
+
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Benchmark script for NMODL LLVM.')
+    parser = argparse.ArgumentParser(description="Benchmark script for NMODL LLVM.")
     # Arguments to initialize BenchmarkConfig
     parser.add_argument(
         "--modfiles",
@@ -174,99 +273,69 @@ def parse_arguments():
         required=True,
     )
     parser.add_argument(
-        "--architectures",
-        nargs="+",
-        help="Architectures to benchmark",
-        required=True
+        "--architectures", nargs="+", help="Architectures to benchmark", required=True
     )
     parser.add_argument(
-        "--compilers",
-        nargs="+",
-        help="Compilers to benchmark",
-        required=True
+        "--compilers", nargs="+", help="Compilers to benchmark", required=True
     )
     parser.add_argument(
         "--external_kernel",
         help="Run external kernel benchmarks",
         action="store_true",
-        required=True
+        required=True,
     )
     parser.add_argument(
-        "--output",
-        help="Output directory for benchmark results",
-        required=True
+        "--output", help="Output directory for benchmark results", required=True
     )
     parser.add_argument(
-        "--modfile_dir",
-        help="Directory containing mod files",
-        required=True
+        "--modfile_dir", help="Directory containing mod files", required=True
     )
     parser.add_argument(
         "--instances",
         type=int,
         default=100000000,
         help="Instances to benchmark",
-        required=False
+        required=False,
     )
     parser.add_argument(
         "--experiments",
         type=int,
-         default=5,
+        default=5,
         help="Experiments to benchmark",
         required=True,
     )
     # Arguments to initialize CompilersConfig
     parser.add_argument(
-        "--svml_lib",
-        type=str,
-        help="SVML library directory to use",
-        required=True
+        "--svml_lib", type=str, help="SVML library directory to use", required=True
     )
     parser.add_argument(
-        "--intel_exe",
-        type=str,
-        help="Intel compiler executable to use",
-        required=True
+        "--intel_exe", type=str, help="Intel compiler executable to use", required=True
     )
     parser.add_argument(
-        "--sleef_lib",
-        type=str,
-        help="Sleef library directory to use",
-        required=True
+        "--sleef_lib", type=str, help="Sleef library directory to use", required=True
     )
     parser.add_argument(
-        "--clang_exe",
-        type=str,
-        help="Clang compiler executable to use",
-        required=True
+        "--clang_exe", type=str, help="Clang compiler executable to use", required=True
     )
     parser.add_argument(
-        "--llc_exe",
-        type=str,
-        help="LLC compiler executable to use",
-        required=True
+        "--llc_exe", type=str, help="LLC compiler executable to use", required=True
     )
     parser.add_argument(
-        "--gcc_exe",
-        type=str,
-        help="GCC compiler executable to use",
-        required=True
+        "--gcc_exe", type=str, help="GCC compiler executable to use", required=True
     )
     parser.add_argument(
         "--libdevice_lib",
         type=str,
         help="Libdevice library directory to use",
-        required=True
+        required=True,
     )
     parser.add_argument(
-        "--nmodl_exe",
-        type=str,
-        help="NMODL executable to use",
-        required=True
+        "--nmodl_exe", type=str, help="NMODL executable to use", required=True
     )
-        
+
     args, _ = parser.parse_known_args()
     return args
+
 
 def main():
     args = parse_arguments()
@@ -278,7 +347,7 @@ def main():
         args.output,
         args.modfile_dir,
         args.instances,
-        args.experiments
+        args.experiments,
     )
     compilers_config = CompilersConfig(
         args.svml_lib,
@@ -288,7 +357,7 @@ def main():
         args.llc_exe,
         args.gcc_exe,
         args.libdevice_lib,
-        args.nmodl_exe
+        args.nmodl_exe,
     )
     benchmark = Benchmark(benchmark_config, compilers_config)
     benchmark.run_benchmark()
