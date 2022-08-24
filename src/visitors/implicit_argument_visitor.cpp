f07fae6c60d6aba3507b7c30ab8eeeec1ba63a1e
Olli Lupton <oliver.lupton@epfl.ch>
2022-08-24 10:03:32 +0200

    implicit argument visitor tests, fixup for at_time with an expression as its first argument

--- src/visitors/implicit_argument_visitor.cpp
+++ src/visitors/implicit_argument_visitor.cpp
@@ -5,12 +5,10 @@
  * Lesser General Public License. See top-level LICENSE file for details.
  *************************************************************************/
 #include "visitors/implicit_argument_visitor.hpp"
+#include "ast/ast_decl.hpp"
 #include "ast/function_call.hpp"
 #include "ast/string.hpp"
 #include "lexer/token_mapping.hpp"
-
-#include <cassert>
-#include <iostream>
 
 namespace nmodl {
 namespace visitor {
@@ -30,11 +28,19 @@ void ImplicitArgumentVisitor::visit_function_call(ast::FunctionCall& node) {
     } else if (nmodl::details::needs_neuron_thread_first_arg(function_name)) {
         // We need to insert `nt` as the first argument if it's not already
         // there
-        // TODO: add a test where the first argument to at_time as some `a+b`
-        // expression and re-add extra logic based on get_node_type() and
-        // AstNodeType::NAME, AstNodeType::STRING, AstNodeType::CONSTANT_VAR,
-        // AstNodeType::VAR_NAME, AstNodeType::LOCAL_VAR.
-        if (arguments.empty() || arguments.front()->get_node_name() != "nt") {
+        auto const is_nt = [](auto const& arg) {
+            // Not all node types implement get_node_name(); for example if the
+            // first argument is `a+b` then calling `get_node_name` would throw
+            auto const node_type = arg.get_node_type();
+            using ast::AstNodeType;
+            if (node_type == AstNodeType::NAME || node_type == AstNodeType::STRING ||
+                node_type == AstNodeType::CONSTANT_VAR || node_type == AstNodeType::VAR_NAME ||
+                node_type == AstNodeType::LOCAL_VAR) {
+                return arg.get_node_name() == "nt";
+            }
+            return false;
+        };
+        if (arguments.empty() || !is_nt(*arguments.front())) {
             auto new_arguments = arguments;
             new_arguments.insert(new_arguments.begin(), std::make_shared<ast::String>("nt"));
             node.set_arguments(std::move(new_arguments));
