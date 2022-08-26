2ac279760737faf0d961d80105490f3d97a5f2ce
Ioannis Magkanaris <ioannis.magkanaris@epfl.ch>
2022-08-26 14:58:52 +0300

    Small improvement in artefact script

--- test/benchmark/artefacts/artefact_script.py
+++ test/benchmark/artefacts/artefact_script.py
@@ -2,10 +2,8 @@ import numpy as np
 import nmodl.dsl as nmodl
 from nmodl import ast, visitor
 
-# read 2 models from files
-hh = open("hh.mod", "r")
-expsyn = open("expsyn.mod", "r")
-models = [hh, expsyn]
+# HH and expsyn mod files
+models = ["hh.mod", "expsyn.mod"]
 
 # parse NMODL mechanisms, create ASTs and find their SUFFIX
 models_ast = []
@@ -13,7 +11,7 @@ models_name = []
 driver = nmodl.NmodlDriver()
 lookup_visitor = visitor.AstLookupVisitor()
 for mod in models:
-    models_ast.append(driver.parse_string(mod.read()))
+    models_ast.append(driver.parse_file(mod))
     models_name.append(lookup_visitor.lookup(models_ast[-1], ast.AstNodeType.SUFFIX)[0].get_node_name())
 
 # code generation and JIT configuration
