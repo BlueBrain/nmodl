6832df95f0ff5e61b20641c3eb2c02e7bbd772d1
Omar Awile <omar.awile@epfl.ch>
2022-09-19 10:51:38 +0200

    Use aligned_alloc as we do in CoreNEURON too
    also updated header includes

--- src/codegen/codegen_c_visitor.cpp
+++ src/codegen/codegen_c_visitor.cpp
@@ -1248,8 +1248,7 @@ void CodegenCVisitor::print_memory_allocation_routine() const {
     printer->add_newline(2);
     auto args = "size_t num, size_t size, size_t alignment = 16";
     printer->fmt_start_block("static inline void* mem_alloc({})", args);
-    printer->add_line("void* ptr;");
-    printer->add_line("posix_memalign(&ptr, alignment, num*size);");
+    printer->add_line("void* ptr = std::aligned_alloc(alignment, num*size);");
     printer->add_line("memset(ptr, 0, size);");
     printer->add_line("return ptr;");
     printer->end_block(1);
@@ -2485,11 +2484,11 @@ void CodegenCVisitor::print_backend_info() {
 
 void CodegenCVisitor::print_standard_includes() {
     printer->add_newline();
-    printer->add_line("#include <math.h>");
+    printer->add_line("#include <cstdlib>");
+    printer->add_line("#include <cmath>");
+    printer->add_line("#include <cstdio>");
+    printer->add_line("#include <cstring>");
     printer->add_line("#include \"nmodl/fast_math.hpp\" // extend math with some useful functions");
-    printer->add_line("#include <stdio.h>");
-    printer->add_line("#include <stdlib.h>");
-    printer->add_line("#include <string.h>");
 }
 
 
