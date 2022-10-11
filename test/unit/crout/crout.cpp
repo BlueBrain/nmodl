219a965a4271c4beca28871106bed09b4673fec0
Christos Kotsalos <christos.kotsalos@epfl.ch>
2022-10-11 18:04:02 +0200

    Remove Eigen::PartialPivLU and replace it with Crout LU decomposition : WIP

--- test/unit/crout/crout.cpp
+++ test/unit/crout/crout.cpp
@@ -34,17 +34,17 @@ bool allclose(const Eigen::DenseBase<DerivedA>& a,
 }
 
 
-bool test_Crout_correctness(double rtol = 1e-8, double atol = 1e-8) {
+template <typename T>
+bool test_Crout_correctness(T rtol = 1e-8, T atol = 1e-8) {
     std::random_device rd;  // seeding
     std::mt19937 mt(rd());
-    std::uniform_real_distribution<double> nums(-1e3, 1e3);
+    std::uniform_real_distribution<T> nums(-1e3, 1e3);
 
     for (int mat_size = 5; mat_size < 15; mat_size++) {
-        Matrix<double, Dynamic, Dynamic, Eigen::ColMajor> A_ColMajor(mat_size,
-                                                                     mat_size);  // default in
-                                                                                 // Eigen!
-        Matrix<double, Dynamic, Dynamic, Eigen::RowMajor> A_RowMajor(mat_size, mat_size);
-        Matrix<double, Dynamic, 1> b(mat_size);
+        Matrix<T, Dynamic, Dynamic, Eigen::ColMajor> A_ColMajor(mat_size,
+                                                                mat_size);  // default in Eigen!
+        Matrix<T, Dynamic, Dynamic, Eigen::RowMajor> A_RowMajor(mat_size, mat_size);
+        Matrix<T, Dynamic, 1> b(mat_size);
 
         for (int repetitions = 0; repetitions < static_cast<int>(1e3); ++repetitions) {
             do {
@@ -59,11 +59,11 @@ bool test_Crout_correctness(double rtol = 1e-8, double atol = 1e-8) {
             } while (!A_ColMajor.fullPivLu().isInvertible());  // Checking Invertibility
 
             // Eigen (ColMajor)
-            Matrix<double, Dynamic, 1> eigen_x_ColMajor(mat_size);
+            Matrix<T, Dynamic, 1> eigen_x_ColMajor(mat_size);
             eigen_x_ColMajor = A_ColMajor.partialPivLu().solve(b);
 
             // Eigen (RowMajor)
-            Matrix<double, Dynamic, 1> eigen_x_RowMajor(mat_size);
+            Matrix<T, Dynamic, 1> eigen_x_RowMajor(mat_size);
             eigen_x_RowMajor = A_RowMajor.partialPivLu().solve(b);
 
             if (!allclose(eigen_x_ColMajor, eigen_x_RowMajor, rtol, atol)) {
@@ -72,12 +72,12 @@ bool test_Crout_correctness(double rtol = 1e-8, double atol = 1e-8) {
             }
 
             // Crout with A_ColMajor
-            Matrix<double, Dynamic, 1> crout_x_ColMajor(mat_size);
+            Matrix<T, Dynamic, 1> crout_x_ColMajor(mat_size);
             if (!A_ColMajor.IsRowMajor)
                 A_ColMajor.transposeInPlace();
             Matrix<int, Dynamic, 1> pivot(mat_size);
-            crout::Crout<double>(mat_size, A_ColMajor.data(), pivot.data());
-            crout::solveCrout<double>(
+            crout::Crout<T>(mat_size, A_ColMajor.data(), pivot.data());
+            crout::solveCrout<T>(
                 mat_size, A_ColMajor.data(), b.data(), crout_x_ColMajor.data(), pivot.data());
 
             if (!allclose(eigen_x_ColMajor, crout_x_ColMajor, rtol, atol)) {
@@ -86,9 +86,9 @@ bool test_Crout_correctness(double rtol = 1e-8, double atol = 1e-8) {
             }
 
             // Crout with A_RowMajor
-            Matrix<double, Dynamic, 1> crout_x_RowMajor(mat_size);
-            crout::Crout<double>(mat_size, A_RowMajor.data(), pivot.data());
-            crout::solveCrout<double>(
+            Matrix<T, Dynamic, 1> crout_x_RowMajor(mat_size);
+            crout::Crout<T>(mat_size, A_RowMajor.data(), pivot.data());
+            crout::solveCrout<T>(
                 mat_size, A_RowMajor.data(), b.data(), crout_x_RowMajor.data(), pivot.data());
 
             if (!allclose(eigen_x_RowMajor, crout_x_RowMajor, rtol, atol)) {
@@ -104,9 +104,11 @@ bool test_Crout_correctness(double rtol = 1e-8, double atol = 1e-8) {
 
 SCENARIO("Compare Crout solver with Eigen") {
     GIVEN("crout (double)") {
-        double rtol = 1e-8;
-        double atol = 1e-8;
-        bool test = test_Crout_correctness(rtol, atol);
+        constexpr double rtol = 1e-8;
+        constexpr double atol = 1e-8;
+
+        auto test = test_Crout_correctness<double>(rtol, atol);
+
         THEN("run tests & compare") {
             REQUIRE(test);
         }
