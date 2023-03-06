c4b3b87fdf416254c7e55d61d082df72d98b66c1
Ioannis Magkanaris <iomagkanaris@gmail.com>
2023-03-06 17:21:23 +0100

    Added unit test for KINETIC block of glia__dbbs_mod_collection__cdp5__CR.mod

--- test/unit/visitor/kinetic_block.cpp
+++ test/unit/visitor/kinetic_block.cpp
@@ -622,4 +622,259 @@ SCENARIO("Convert KINETIC to DERIVATIVE using KineticBlock visitor", "[kinetic][
             REQUIRE(result[0] == reindent_text(output_nmodl_text));
         }
     }
+    GIVEN("Complicated KINETIC block from DBBS") {
+        std::string input_nmodl_text = R"(
+            NEURON {
+            SUFFIX glia__dbbs_mod_collection__cdp5__CR
+            USEION ca READ cao, cai, ica WRITE cai
+            RANGE ica_pmp
+            RANGE Nannuli, Buffnull2, rf3, rf4, vrat, cainull, CR, CR_1C_0N, CR_2C_2N, CR_1V, CRnull
+            RANGE TotalPump
+            RANGE dsq, dsqvol
+
+            }
+
+
+            UNITS {
+                (mol)   = (1)
+                (molar) = (1/liter)
+                (mM)    = (millimolar)
+                (um)    = (micron)
+                (mA)    = (milliamp)
+                FARADAY = (faraday)  (10000 coulomb)
+                PI      = (pi)       (1)
+            }
+
+            PARAMETER {
+                Nannuli = 10.9495 (1)
+                celsius (degC)
+                    
+                cainull = 45e-6 (mM)
+                    mginull =.59    (mM)
+
+            :	values for a buffer compensating the diffusion
+
+                Buffnull1 = 0	(mM)
+                rf1 = 0.0134329	(/ms mM)
+                rf2 = 0.0397469	(/ms)
+
+                Buffnull2 = 60.9091	(mM)
+                rf3 = 0.1435	(/ms mM)
+                rf4 = 0.0014	(/ms)
+
+            :	values for benzothiazole coumarin (BTC)
+                BTCnull = 0	(mM)
+                b1 = 5.33	(/ms mM)
+                b2 = 0.08	(/ms)
+
+            :	values for caged compound DMNPE-4
+                DMNPEnull = 0	(mM)
+                c1 = 5.63	(/ms mM)
+                c2 = 0.107e-3	(/ms)
+
+            :       values for Calretinin (6 sites but only 5 active) (2*2 cooperative sites and 1 single indipendent site)
+
+                    CRnull =	0.9             (mM):0.7-1.2
+                    nT1   = 1.8            (/ms mM)
+                    nT2   = 0.053        (/ms)
+                    nR1   = 310           (/ms mM)
+                    nR2   = 0.02        (/ms)
+                    
+                nV1   = 7.3            (/ms mM)
+                    nV2   = 0.24        (/ms)
+                    
+                    :pumps
+
+                kpmp1    = 3e-3       (/mM-ms)
+                kpmp2    = 1.75e-5   (/ms)
+                kpmp3    = 7.255e-5  (/ms)
+                TotalPump = 1e-9	(mol/cm2)	
+
+            }
+
+            ASSIGNED {
+                diam      (um)
+                ica       (mA/cm2)
+                ica_pmp   (mA/cm2)
+                parea     (um)     : pump area per unit length
+                parea2	  (um)
+                cai       (mM)
+                cao       (mM)
+                mgi	(mM)
+                vrat	(1)
+                dsq
+                dsqvol	
+            }
+
+            STATE {
+                : ca[0] is equivalent to cai
+                : ca[] are very small, so specify absolute tolerance
+                : let it be ~1.5 - 2 orders of magnitude smaller than baseline level
+
+                ca		(mM)    <1e-3>
+                mg		(mM)	<1e-6>
+                
+                Buff1		(mM)	
+                Buff1_ca	(mM)
+
+                Buff2		(mM)
+                Buff2_ca	(mM)
+
+                BTC		(mM)
+                BTC_ca		(mM)
+
+                DMNPE		(mM)
+                DMNPE_ca	(mM)	
+                    
+                    :calretinin
+                    
+                CR		(mM)
+                
+                    CR_1C_0N	(mM)
+                CR_2C_0N	(mM)  
+                CR_2C_1N	(mM)
+                
+                CR_1C_1N	(mM)
+
+                CR_0C_1N	(mM)
+                CR_0C_2N	(mM)
+                CR_1C_2N	(mM)
+                
+                CR_2C_2N	(mM)
+                
+                CR_1V 		(mM)
+
+                
+
+                :pumps
+                
+                pump		(mol/cm2) <1e-15>
+                pumpca		(mol/cm2) <1e-15>
+
+            }
+
+            BREAKPOINT {
+                SOLVE state METHOD sparse
+            }
+
+            KINETIC state {
+            COMPARTMENT diam*diam*vrat {ca mg Buff1 Buff1_ca Buff2 Buff2_ca BTC BTC_ca DMNPE DMNPE_ca CR CR_1C_0N CR_2C_0N CR_2C_1N CR_0C_1N CR_0C_2N CR_1C_2N CR_1C_1N CR_2C_1N CR_1C_2N CR_2C_2N}
+            COMPARTMENT (1e10)*parea {pump pumpca}
+
+
+                :pump
+                ~ ca + pump <-> pumpca  (kpmp1*parea*(1e10), kpmp2*parea*(1e10))
+                ~ pumpca <-> pump   (kpmp3*parea*(1e10), 0)
+                CONSERVE pump + pumpca = TotalPump * parea * (1e10)
+                
+                ica_pmp = 2*FARADAY*(f_flux - b_flux)/parea	
+                : all currents except pump
+                : ica is Ca efflux
+                ~ ca << (-ica*PI*diam/(2*FARADAY))
+
+                :RADIAL DIFFUSION OF ca, mg and mobile buffers
+
+                dsq = diam*diam
+                    dsqvol = dsq*vrat
+                    ~ ca + Buff1 <-> Buff1_ca (rf1*dsqvol, rf2*dsqvol)
+                    ~ ca + Buff2 <-> Buff2_ca (rf3*dsqvol, rf4*dsqvol)
+                    ~ ca + BTC <-> BTC_ca (b1*dsqvol, b2*dsqvol)
+                    ~ ca + DMNPE <-> DMNPE_ca (c1*dsqvol, c2*dsqvol)
+                        
+                        :Calretinin
+                        :Slow state
+                    ~ ca + CR <-> CR_1C_0N (nT1*dsqvol, nT2*dsqvol)
+                        ~ ca + CR_1C_0N <-> CR_2C_0N (nR1*dsqvol, nR2*dsqvol)
+                        ~ ca + CR_2C_0N <-> CR_2C_1N (nT1*dsqvol, nT2*dsqvol)
+                        
+                        :fast state
+                    ~ ca + CR <-> CR_0C_1N (nT1*dsqvol, nT2*dsqvol)
+                    ~ ca + CR_0C_1N <-> CR_0C_2N (nR1*dsqvol, nR2*dsqvol)
+                    ~ ca + CR_0C_2N <-> CR_1C_2N (nT1*dsqvol, nT2*dsqvol)
+                    
+                        :complete
+                        ~ ca + CR_2C_1N <-> CR_2C_2N (nR1*dsqvol, nR2*dsqvol)
+                        ~ ca + CR_1C_2N <-> CR_2C_2N (nR1*dsqvol, nR2*dsqvol)
+                        
+                        :mixed
+                        ~ ca + CR_1C_0N <-> CR_1C_1N (nT1*dsqvol, nT2*dsqvol)   
+                    ~ ca + CR_0C_1N <-> CR_1C_1N (nT1*dsqvol, nT2*dsqvol) 
+                    
+                    ~ ca + CR_1C_1N <-> CR_2C_1N (nR1*dsqvol, nR2*dsqvol)   
+                    ~ ca + CR_1C_1N <-> CR_1C_2N (nR1*dsqvol, nR2*dsqvol) 
+                        
+                        :Fith site
+                        ~ ca + CR  <-> CR_1V	     (nV1*dsqvol, nV2*dsqvol)
+                        
+
+                        
+                cai = ca
+                mgi = mg
+            }
+
+            FUNCTION ssBuff1() (mM) {
+                ssBuff1 = Buffnull1/(1+((rf1/rf2)*cainull))
+            }
+            FUNCTION ssBuff1ca() (mM) {
+                ssBuff1ca = Buffnull1/(1+(rf2/(rf1*cainull)))
+            }
+            FUNCTION ssBuff2() (mM) {
+                    ssBuff2 = Buffnull2/(1+((rf3/rf4)*cainull))
+            }
+            FUNCTION ssBuff2ca() (mM) {
+                    ssBuff2ca = Buffnull2/(1+(rf4/(rf3*cainull)))
+            }
+
+            FUNCTION ssBTC() (mM) {
+                ssBTC = BTCnull/(1+((b1/b2)*cainull))
+            }
+
+            FUNCTION ssBTCca() (mM) {
+                ssBTCca = BTCnull/(1+(b2/(b1*cainull)))
+            }
+
+            FUNCTION ssDMNPE() (mM) {
+                ssDMNPE = DMNPEnull/(1+((c1/c2)*cainull))
+            }
+
+            FUNCTION ssDMNPEca() (mM) {
+                ssDMNPEca = DMNPEnull/(1+(c2/(c1*cainull)))
+            })";
+        std::string output_nmodl_text = R"(
+            DERIVATIVE state {
+                CONSERVE pumpca = (TotalPump*parea*1e10-(1e10*parea*1*pump))/(1e10*parea*1)
+                ica_pmp = 2*FARADAY*(kpmp3*parea*1e10*pumpca-0*pump)/parea
+                dsq = diam*diam
+                dsqvol = dsq*vrat
+                cai = ca
+                mgi = mg
+                ca' = ((-ica*PI*diam/(2*FARADAY))+(-1*(kpmp1*parea*1e10*ca*pump-kpmp2*parea*1e10*pumpca))+(-1*(rf1*dsqvol*ca*Buff1-rf2*dsqvol*Buff1_ca))+(-1*(rf3*dsqvol*ca*Buff2-rf4*dsqvol*Buff2_ca))+(-1*(b1*dsqvol*ca*BTC-b2*dsqvol*BTC_ca))+(-1*(c1*dsqvol*ca*DMNPE-c2*dsqvol*DMNPE_ca))+(-1*(nT1*dsqvol*ca*CR-nT2*dsqvol*CR_1C_0N))+(-1*(nR1*dsqvol*ca*CR_1C_0N-nR2*dsqvol*CR_2C_0N))+(-1*(nT1*dsqvol*ca*CR_2C_0N-nT2*dsqvol*CR_2C_1N))+(-1*(nT1*dsqvol*ca*CR-nT2*dsqvol*CR_0C_1N))+(-1*(nR1*dsqvol*ca*CR_0C_1N-nR2*dsqvol*CR_0C_2N))+(-1*(nT1*dsqvol*ca*CR_0C_2N-nT2*dsqvol*CR_1C_2N))+(-1*(nR1*dsqvol*ca*CR_2C_1N-nR2*dsqvol*CR_2C_2N))+(-1*(nR1*dsqvol*ca*CR_1C_2N-nR2*dsqvol*CR_2C_2N))+(-1*(nT1*dsqvol*ca*CR_1C_0N-nT2*dsqvol*CR_1C_1N))+(-1*(nT1*dsqvol*ca*CR_0C_1N-nT2*dsqvol*CR_1C_1N))+(-1*(nR1*dsqvol*ca*CR_1C_1N-nR2*dsqvol*CR_2C_1N))+(-1*(nR1*dsqvol*ca*CR_1C_1N-nR2*dsqvol*CR_1C_2N))+(-1*(nV1*dsqvol*ca*CR-nV2*dsqvol*CR_1V)))/(diam*diam*vrat)
+                CR' = ((-1*(nT1*dsqvol*ca*CR-nT2*dsqvol*CR_1C_0N))+(-1*(nT1*dsqvol*ca*CR-nT2*dsqvol*CR_0C_1N))+(-1*(nV1*dsqvol*ca*CR-nV2*dsqvol*CR_1V)))/(diam*diam*vrat)
+                CR_1C_0N' = ((1*(nT1*dsqvol*ca*CR-nT2*dsqvol*CR_1C_0N))+(-1*(nR1*dsqvol*ca*CR_1C_0N-nR2*dsqvol*CR_2C_0N))+(-1*(nT1*dsqvol*ca*CR_1C_0N-nT2*dsqvol*CR_1C_1N)))/(diam*diam*vrat)
+                CR_2C_2N' = ((1*(nR1*dsqvol*ca*CR_2C_1N-nR2*dsqvol*CR_2C_2N))+(1*(nR1*dsqvol*ca*CR_1C_2N-nR2*dsqvol*CR_2C_2N)))/(diam*diam*vrat)
+                CR_1V' = (1*(nV1*dsqvol*ca*CR-nV2*dsqvol*CR_1V))
+                mg' = (1)/(diam*diam*vrat)
+                Buff1' = ((-1*(rf1*dsqvol*ca*Buff1-rf2*dsqvol*Buff1_ca)))/(diam*diam*vrat)
+                Buff1_ca' = ((1*(rf1*dsqvol*ca*Buff1-rf2*dsqvol*Buff1_ca)))/(diam*diam*vrat)
+                Buff2' = ((-1*(rf3*dsqvol*ca*Buff2-rf4*dsqvol*Buff2_ca)))/(diam*diam*vrat)
+                Buff2_ca' = ((1*(rf3*dsqvol*ca*Buff2-rf4*dsqvol*Buff2_ca)))/(diam*diam*vrat)
+                BTC' = ((-1*(b1*dsqvol*ca*BTC-b2*dsqvol*BTC_ca)))/(diam*diam*vrat)
+                BTC_ca' = ((1*(b1*dsqvol*ca*BTC-b2*dsqvol*BTC_ca)))/(diam*diam*vrat)
+                DMNPE' = ((-1*(c1*dsqvol*ca*DMNPE-c2*dsqvol*DMNPE_ca)))/(diam*diam*vrat)
+                DMNPE_ca' = ((1*(c1*dsqvol*ca*DMNPE-c2*dsqvol*DMNPE_ca)))/(diam*diam*vrat)
+                CR_2C_0N' = ((1*(nR1*dsqvol*ca*CR_1C_0N-nR2*dsqvol*CR_2C_0N))+(-1*(nT1*dsqvol*ca*CR_2C_0N-nT2*dsqvol*CR_2C_1N)))/(diam*diam*vrat)
+                CR_2C_1N' = ((1*(nT1*dsqvol*ca*CR_2C_0N-nT2*dsqvol*CR_2C_1N))+(-1*(nR1*dsqvol*ca*CR_2C_1N-nR2*dsqvol*CR_2C_2N))+(1*(nR1*dsqvol*ca*CR_1C_1N-nR2*dsqvol*CR_2C_1N)))/(diam*diam*vrat)
+                CR_1C_1N' = ((1*(nT1*dsqvol*ca*CR_1C_0N-nT2*dsqvol*CR_1C_1N))+(1*(nT1*dsqvol*ca*CR_0C_1N-nT2*dsqvol*CR_1C_1N))+(-1*(nR1*dsqvol*ca*CR_1C_1N-nR2*dsqvol*CR_2C_1N))+(-1*(nR1*dsqvol*ca*CR_1C_1N-nR2*dsqvol*CR_1C_2N)))/(diam*diam*vrat)
+                CR_0C_1N' = ((1*(nT1*dsqvol*ca*CR-nT2*dsqvol*CR_0C_1N))+(-1*(nR1*dsqvol*ca*CR_0C_1N-nR2*dsqvol*CR_0C_2N))+(-1*(nT1*dsqvol*ca*CR_0C_1N-nT2*dsqvol*CR_1C_1N)))/(diam*diam*vrat)
+                CR_0C_2N' = ((1*(nR1*dsqvol*ca*CR_0C_1N-nR2*dsqvol*CR_0C_2N))+(-1*(nT1*dsqvol*ca*CR_0C_2N-nT2*dsqvol*CR_1C_2N)))/(diam*diam*vrat)
+                CR_1C_2N' = ((1*(nT1*dsqvol*ca*CR_0C_2N-nT2*dsqvol*CR_1C_2N))+(-1*(nR1*dsqvol*ca*CR_1C_2N-nR2*dsqvol*CR_2C_2N))+(1*(nR1*dsqvol*ca*CR_1C_1N-nR2*dsqvol*CR_1C_2N)))/(diam*diam*vrat)
+                pump' = ((-1*(kpmp1*parea*1e10*ca*pump-kpmp2*parea*1e10*pumpca))+(1*(kpmp3*parea*1e10*pumpca-0*pump)))/(1e10*parea)
+                pumpca' = ((1*(kpmp1*parea*1e10*ca*pump-kpmp2*parea*1e10*pumpca))+(-1*(kpmp3*parea*1e10*pumpca-0*pump)))/(1e10*parea)
+            })";
+        THEN("Convert to equivalent DERIVATIVE block") {
+            auto result = run_kinetic_block_visitor(input_nmodl_text);
+            CAPTURE(input_nmodl_text);
+            REQUIRE(result[0] == reindent_text(output_nmodl_text));
+        }
+    }
 }
