d0a73dd3c36a8e16f8c6e6f1e68028fd854da592
Nicolas Cornu <nicolas.cornu@epfl.ch>
2023-04-20 16:46:24 +0200

    Add new tests for USEION

--- test/unit/codegen/codegen_cpp_visitor.cpp
+++ test/unit/codegen/codegen_cpp_visitor.cpp
@@ -1188,7 +1188,7 @@ SCENARIO("nrn_state should not be always written", "[codegen][nrn_state]") {
             REQUIRE_THAT(generated, Contains(expected_code));
         }
     }
-    GIVEN("A mod file with a SOLVE inside a BREAKPOINT") {
+    GIVEN("A mod file with a STATE block") {
         std::string const nmodl_text = R"(
             STATE {
                 pump (mol/cm2) <1.e-3>
@@ -1199,6 +1199,19 @@ SCENARIO("nrn_state should not be always written", "[codegen][nrn_state]") {
             std::string expected_code =
                 R"(void nrn_state_(NrnThread* nt, Memb_list* ml, int type) {)";
             REQUIRE_THAT(generated, Contains(expected_code));
+        }
+    }
+    GIVEN("A mod file with USEION statement with READ and WRITE") {
+        std::string const nmodl_text = R"(
+            NEURON {
+                USEION na READ ena WRITE ina
+            }
+        )";
+        THEN("nrn_state should not be generated") {
+            auto const generated = get_cpp_code(nmodl_text);
+            std::string expected_code =
+                R"(void nrn_state_(NrnThread* nt, Memb_list* ml, int type) {)";
+            REQUIRE_THAT(generated, !Contains(expected_code));
         }
     }
 }
