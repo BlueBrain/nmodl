226e9aefe9996d0483716cef9ed3d47d486f878d
Nicolas Cornu <nicolas.cornu@epfl.ch>
2023-05-17 17:20:02 +0200

    Format

--- test/unit/newton/newton.cpp
+++ test/unit/newton/newton.cpp
@@ -7,8 +7,8 @@
 
 #include "nmodl.hpp"
 
-#include <catch2/matchers/catch_matchers_floating_point.hpp>
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 
 #include <cmath>
 
