5b3dbfd994439882684d8618bce4c40cb735b6ba
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2024-02-17 09:39:26 +0100

    Add testing using cibuildwheel

--- packaging/test_wheel.bash
+++ packaging/test_wheel.bash
@@ -2,69 +2,26 @@
 # A simple set of tests checking if a wheel is working correctly
 set -xe
 
-if ! [ -f setup.py ] && ! [ -f pyproject.toml ]; then
-    echo "Error: Please launch $0 from the root dir"
+
+if [ -z "${VIRTUAL_ENV}" ]
+then
+    echo "ERROR: you do not appear to be in a virtual environment, please use one before running tests"
     exit 1
 fi
-
-if [ "$#" -lt 2 ]; then
-    echo "Usage: $(basename $0) python_exe python_wheel [use_virtual_env]"
-    exit 1
-fi
-
-# cli parameters
-python_exe=$1
-python_wheel=$2
-use_venv=$3 #if $3 is not "false" then use virtual environment
-
-python_ver=$("$python_exe" -c "import sys; print('%d%d' % tuple(sys.version_info)[:2])")
-
 
 test_wheel () {
+    # the path to the root directory
+    rootdir="$1"
     # sample mod file for nrnivmodl check
-    local TEST_DIR="test_dir" 
-    mkdir -p $TEST_DIR
-    cp ../nmodl/ext/example/*.mod $TEST_DIR/
-    cp ../test/integration/mod/cabpump.mod ../test/integration/mod/var_init.inc $TEST_DIR/
-    cd $TEST_DIR
-    for mod in *.mod
+    test_dir="$(mktemp -d)"
+    cp "${rootdir}"/nmodl/ext/example/*.mod "${test_dir}"
+    cp "${rootdir}/test/integration/mod/cabpump.mod" "${rootdir}/test/integration/mod/var_init.inc" "${test_dir}"
+    for mod in "${test_dir}"/*.mod
     do
-        nmodl $mod sympy --analytic
+        nmodl "$mod" sympy --analytic
     done
-    $python_exe -c "import nmodl; driver = nmodl.NmodlDriver(); driver.parse_file('hh.mod')"
-    cd ..
-    #clean-up
-    rm -rf $TEST_DIR
+    python -c "import nmodl; driver = nmodl.NmodlDriver(); driver.parse_file('${test_dir}/hh.mod')"
 }
 
-echo "== Testing $python_wheel using $python_exe ($python_ver) =="
-
-mkdir testwheel && cd testwheel
-
-# creat python virtual environment and use `python` as binary name
-# because it will be correct one from venv.
-if [[ "$use_venv" != "false" ]]; then
-  echo " == Creating virtual environment == "
-  venv_name="nmodl_test_venv_${python_ver}"
-  $python_exe -m venv $venv_name
-  . $venv_name/bin/activate
-  python_exe=`which python`
-else
-  echo " == Using global install == "
-fi
-
-# install nmodl
-$python_exe -m pip install -U pip
-$python_exe -m pip install ../$python_wheel
-$python_exe -m pip show nmodl || $python_exe -m pip show nmodl-nightly
-
 # run tests
-test_wheel $(which python)
-
-# cleanup
-if [[ "$use_venv" != "false" ]]; then
-  deactivate
-fi
-
-rm -rf $venv_name
-echo "Removed $venv_name"
+test_wheel "$@"
