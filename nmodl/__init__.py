02191dea4dba79a5d2ec8b4260cfddd4a37a4f19
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2024-02-20 09:38:20 +0100

    Fix for NMODL sympy visitor
    When using the sympy visitor, the NMODL Python module by default throws
    an error that `NMODL_PYLIB` is not set. The solution is to set all of
    these env variables as soon as we load the module.

--- nmodl/__init__.py
+++ nmodl/__init__.py
@@ -1,8 +1,25 @@
+import os
+import sys
+
+if sys.version_info >= (3, 9):
+    from importlib.resources import files
+    from importlib.metadata import version, PackageNotFoundError
+else:
+    from importlib_resources import files
+    from importlib_metadata import version, PackageNotFoundError
+
+from find_libpython import find_libpython
+
+# add libpython*.so path to environment
+os.environ["NMODL_PYLIB"] = find_libpython()
+
+# add nmodl home to environment (i.e. necessary for nrnunits.lib)
+os.environ["NMODLHOME"] = str(files("nmodl") / ".data")
+
+
 try:
     # Try importing but catch exception in case bindings are not available
     from ._nmodl import NmodlDriver, to_json, to_nmodl  # noqa
-
-    from importlib.metadata import version, PackageNotFoundError
 
     try:
         __version__ = version("nmodl")
