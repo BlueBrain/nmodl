e54ee01d649d11cec838170ff71675b2b18f0491
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2024-02-22 10:55:54 +0100

    WIP on azure pipeline Linux testing

--- packaging/test_wheel.bash
+++ packaging/test_wheel.bash
@@ -2,6 +2,8 @@
 # A simple set of tests checking if a wheel is working correctly
 set -xe
 
+python -m pytest "$1/test/unit/pybind"
 find "$1/test/" "$1/nmodl/ext/" \
     -name "*.mod" \
+    -exec nmodl '{}' sympy --analytic \; \
     -exec python -c "import nmodl; driver = nmodl.NmodlDriver(); driver.parse_file('{}')" \;
