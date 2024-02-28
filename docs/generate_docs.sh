b1afcd0923d6c10f86f86dbbb841a8df289fa0cb
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2024-02-28 17:03:15 +0100

    Fix doxygen issues when building docs

--- docs/generate_docs.sh
+++ docs/generate_docs.sh
@@ -6,8 +6,13 @@
 
 set -xeu
 
+# in order to create the docs, we first need to build NMODL
+build_dir="$(mktemp -d)"
+wheel_dir="$(mktemp -d)"
+pip wheel . --no-deps --wheel-dir "$(cd "${wheel_dir}"; pwd -P)" -C build-dir="$(cd "${build_dir}"; pwd -P)"
+
 # the abs dir where this script is located (so we can call it from wherever)
-script_dir="$(realpath "$(dirname "$0")")"
+script_dir="$(cd "$(dirname "$0")"; pwd -P)"
 
 cd "${script_dir}"
 doxygen Doxyfile
