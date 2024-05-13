6ab4928ebb6bcd14c56b19870ad0a5b0e7ddd736
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-05-13 15:28:27 +0200

    Attempt 18.

--- test/usecases/generate_references.sh
+++ test/usecases/generate_references.sh
@@ -11,5 +11,21 @@ nmodl="$1"
 usecase_dir="$2"
 output_dir="$3"
 
-"${nmodl}" "${usecase_dir}"/*.mod --neuron -o "${output_dir}"/neuron
+function sanitize() {
+  for f in "${1}"/*.cpp
+  do
+    if [[ "$(uname)" == 'Darwin' ]]; then
+        sed_cmd=("sed" "-i" "")
+    else
+        sed_cmd=("sed" "-i")
+    fi
+    "${sed_cmd[@]}" "s/Created         : .*$/Created         : DATE/" "$f"
+    "${sed_cmd[@]}" "s/NMODL Compiler  : .*$/NMODL Compiler  : VERSION/" "$f"
+  done
+}
+
+"${nmodl}" "${usecase_dir}"/*.mod --neuron -o "${output_dir}/neuron"
+sanitize "${output_dir}/neuron"
+
 "${nmodl}" "${usecase_dir}"/*.mod -o "${output_dir}"/coreneuron
+sanitize "${output_dir}/coreneuron"
