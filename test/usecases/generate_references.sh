25c39c616bc2f847becbbe1a3ec44831b47f025c
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-05-13 14:08:26 +0200

    First try.

--- test/usecases/generate_references.sh
+++ test/usecases/generate_references.sh
@@ -1,32 +1,15 @@
 #! /usr/bin/env bash
 set -eu
 
-nmodl="$1"
-usecase_dir="$2"
-
-if [[ $# -eq 3 ]]
+if [[ $# -ne 3 ]]
 then
-  output_dir="$3"
-else
-  script_dir="$(cd "$(dirname "$0")"; pwd -P)"
-  output_dir="${script_dir}/references/$(basename "$2")"
+  echo "Usage: $0 NMODL USECASE_DIR OUTPUT_DIR"
+  exit -1
 fi
 
-function sanitize() {
-  for f in "${1}"/*.cpp
-  do
-    if [[ "$(uname)" == 'Darwin' ]]; then
-        sed_cmd=("sed" "-i" "")
-    else
-        sed_cmd=("sed" "-i")
-    fi
-    "${sed_cmd[@]}" "s/Created         : .*$/Created         : DATE/" "$f"
-    "${sed_cmd[@]}" "s/NMODL Compiler  : .*$/NMODL Compiler  : VERSION/" "$f"
-  done
-}
+nmodl="$1"
+usecase_dir="$2"
+output_dir="$3"
 
-"${nmodl}" "${usecase_dir}"/*.mod --neuron -o "${output_dir}/neuron"
-sanitize "${output_dir}/neuron"
-
+"${nmodl}" "${usecase_dir}"/*.mod --neuron -o "${output_dir}"/neuron
 "${nmodl}" "${usecase_dir}"/*.mod -o "${output_dir}"/coreneuron
-sanitize "${output_dir}/coreneuron"
