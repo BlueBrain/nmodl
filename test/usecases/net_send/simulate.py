58fb270a48adb1b4b0fa74792518fdcdb42af73f
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-05-14 16:08:14 +0200

    Test artificial cell `net_send`.

--- test/usecases/net_send/simulate.py
+++ test/usecases/net_send/simulate.py
@@ -3,23 +3,51 @@ import numpy as np
 from neuron import h, gui
 from neuron.units import ms
 
-nseg = 1
 
-s = h.Section()
-s.nseg = nseg
+def simulate():
+    nseg = 1
 
-toggle = h.toggle(s(0.5))
+    s = h.Section()
+    s.nseg = nseg
 
-y_hoc = h.Vector().record(toggle._ref_y)
-t_hoc = h.Vector().record(h._ref_t)
+    toggle = h.toggle(s(0.5))
+    art_toggle = h.art_toggle()
 
-h.stdinit()
-h.tstop = 5.0 * ms
-h.run()
+    t_hoc = h.Vector().record(h._ref_t)
+    y_hoc = h.Vector().record(toggle._ref_y)
+    art_y_hoc = h.Vector().record(art_toggle._ref_y)
 
-t = np.array(t_hoc.as_numpy())
-y = np.array(y_hoc.as_numpy())
+    h.stdinit()
+    h.tstop = 5.0 * ms
+    h.run()
 
-y_exact = np.array(t >= 2.0, np.float64)
+    t = np.array(t_hoc.as_numpy())
+    y = np.array(y_hoc.as_numpy())
+    art_y = np.array(art_y_hoc.as_numpy())
 
-assert np.all(np.abs(y - y_exact) == 0), f"{y} != {y_exact}, delta: {y - y_exact}"
+    return t, y, art_y
+
+
+def check_solution(t, y, art_y):
+    y_exact = np.array(t >= 2.0, np.float64)
+    art_y_exact = np.array(t >= 1.5, np.float64)
+
+    assert np.all(np.abs(y - y_exact) == 0), f"{y} != {y_exact}, delta: {y - y_exact}"
+    assert np.all(
+        np.abs(art_y - art_y_exact) == 0
+    ), f"{art_y} != {art_y_exact}, delta: {art_y - art_y_exact}"
+
+
+def plot_solution(t, y, art_y):
+    import matplotlib.pyplot as plt
+
+    plt.plot(t, y)
+    plt.plot(t, art_y)
+    plt.show()
+
+
+if __name__ == "__main__":
+    t, y, art_y = simulate()
+    check_solution(t, y, art_y)
+
+    # plot_solution(t, y, art_y)
