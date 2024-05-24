ce194562956d548718b8cf191eabbaa0914f1721
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2024-05-24 17:44:57 +0200

    Fix recursive function calls

--- test/usecases/recursion/simulate.py
+++ test/usecases/recursion/simulate.py
@@ -5,20 +5,30 @@ from neuron import gui
 from neuron import h
 
 
+def naive_fib(n: int):
+    if n in [0, 1]:
+        return n
+    return naive_fib(n - 1) + naive_fib(n - 2)
+
+
 def simulate():
     s = h.Section(name="soma")
     s.L = 10
     s.diam = 10
     s.insert("recursion")
-    return s(0.5).recursion.myfactorial
+    fact = s(0.5).recursion.myfactorial
+    fib = s(0.5).recursion.myfib
+
+    return fact, fib
 
 
-def check_solution(f):
+def check_solution(f, reference):
     for n in range(10):
-        exact, expected = math.factorial(n), f(n)
+        exact, expected = reference(n), f(n)
         assert np.isclose(exact, expected), f"{expected} != {exact}"
 
 
 if __name__ == "__main__":
-    f = simulate()
-    check_solution(f)
+    fact, fib = simulate()
+    check_solution(fact, math.factorial)
+    check_solution(fib, naive_fib)
