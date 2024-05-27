c54a85c15618b17fb4271d27ce2a175b0144a460
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2024-05-27 09:22:43 +0200

    Refactor tests

--- test/usecases/recursion/recursion.mod
+++ test/usecases/recursion/recursion.mod
@@ -2,21 +2,6 @@ NEURON {
     SUFFIX recursion
 }
 
-: single recursive call
-FUNCTION f_simple(n) {
-    f_simple = f_simple(n)
-}
-
-: multiple recursive calls
-FUNCTION fc(n) {
-    fc = fc(fc(fc(fc(n))))
-}
-
-: multiple recursive calls with different args
-FUNCTION fd(n) {
-    fd = fd(fc(fd(n) + fc(fd(n))))
-}
-
 FUNCTION myfactorial(n) {
     if (n == 0 || n == 1) {
         myfactorial = 1
@@ -24,11 +9,3 @@ FUNCTION myfactorial(n) {
         myfactorial = n * myfactorial(n - 1)
     }
 } 
-
-FUNCTION myfib(n) {
-    if (n == 0 || n == 1){
-        myfib = n
-    } else {
-        myfib = myfib(n - 1) + myfib(n - 2)
-    }
-}
--- test/usecases/recursion/simulate.py
+++ test/usecases/recursion/simulate.py
@@ -5,21 +5,14 @@ from neuron import gui
 from neuron import h
 
 
-def naive_fib(n: int):
-    if n in [0, 1]:
-        return n
-    return naive_fib(n - 1) + naive_fib(n - 2)
-
-
 def simulate():
     s = h.Section(name="soma")
     s.L = 10
     s.diam = 10
     s.insert("recursion")
     fact = s(0.5).recursion.myfactorial
-    fib = s(0.5).recursion.myfib
 
-    return fact, fib
+    return fact
 
 
 def check_solution(f, reference):
@@ -29,6 +22,5 @@ def check_solution(f, reference):
 
 
 if __name__ == "__main__":
-    fact, fib = simulate()
+    fact = simulate()
     check_solution(fact, math.factorial)
-    check_solution(fib, naive_fib)
