09e4c5a42609111aa64db8ea5e842d30fa97f454
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-06-03 09:08:17 +0200

    Also run any `test_*.py` files.

--- test/usecases/run_test.sh
+++ test/usecases/run_test.sh
@@ -1,9 +1,20 @@
 #! /usr/bin/env bash
 set -eu
 
+function run_tests() {
+  for f in test_*.py simulate.py
+  do
+    if [[ -f "$f" ]]
+    then
+      python "$f"
+    fi
+  done
+}
+
 if [[ $# -ne 2 ]]
 then
   echo "Usage: $0 NMODL USECASE_DIR"
+  exit -1
 fi
 
 nmodl="$1"
@@ -16,13 +27,13 @@ pushd "${usecase_dir}"
 echo "-- Running NRN+nocmodl ------"
 rm -r "${output_dir}" tmp || true
 nrnivmodl
-"$(uname -m)/special" -nogui simulate.py
+run_tests
 
 
 # NRN + NMODL
 echo "-- Running NRN+NMODL --------"
 rm -r "${output_dir}" tmp || true
 nrnivmodl -nmodl "${nmodl}"
-"$(uname -m)/special" -nogui simulate.py
+run_tests
 
 popd
