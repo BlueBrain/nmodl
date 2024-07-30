4d68b6b18b1903d3ced44428b4d20790166c320f
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-07-30 16:33:39 +0200

    Move `print_top_verbatim_blocks` upwards.
    Both the NEURON and CoreNEURON codegen need the same code for printing
    all top-level verbatim blocks.

--- src/codegen/codegen_cpp_visitor.hpp
+++ src/codegen/codegen_cpp_visitor.hpp
@@ -1449,6 +1449,11 @@ class CodegenCppVisitor: public visitor::ConstAstVisitor {
      */
     void print_nmodl_constants();
 
+    /**
+     * Print top level (global scope) verbatim blocks
+     */
+    void print_top_verbatim_blocks();
+
 
     /****************************************************************************************/
     /*                            Overloaded visitor routines                               */
