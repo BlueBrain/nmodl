b87d1f8fbb6820224eec9447f0617daa1bd0b11d
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-09-17 17:11:12 +0200

    Remove noisy `static_cast<int>(0)`.

--- src/codegen/codegen_cpp_visitor.cpp
+++ src/codegen/codegen_cpp_visitor.cpp
@@ -855,9 +855,7 @@ void CodegenCppVisitor::visit_var_name(const VarName& node) {
     }
     if (index) {
         printer->add_text("[");
-        printer->add_text("static_cast<int>(");
         index->accept(*this);
-        printer->add_text(")");
         printer->add_text("]");
     }
 }
@@ -866,9 +864,7 @@ void CodegenCppVisitor::visit_var_name(const VarName& node) {
 void CodegenCppVisitor::visit_indexed_name(const IndexedName& node) {
     node.get_name()->accept(*this);
     printer->add_text("[");
-    printer->add_text("static_cast<int>(");
     node.get_length()->accept(*this);
-    printer->add_text(")");
     printer->add_text("]");
 }
 
