2c462e84cef8b3fddfc5c4f22a95d0bd5f60a6ea
1uc <luc.grosheintz@gmail.com>
2024-09-23 14:55:26 +0000

    Deploying to gh-pages from @ BlueBrain/nmodl@1e0db745c82f78ca06bfbb18716eccda8cd5353a 🚀

--- doxygen/classnmodl_1_1codegen_1_1_codegen_neuron_cpp_visitor.js
+++ doxygen/classnmodl_1_1codegen_1_1_codegen_neuron_cpp_visitor.js
@@ -28,7 +28,7 @@ var classnmodl_1_1codegen_1_1_codegen_neuron_cpp_visitor =
     [ "position_of_int_var", "classnmodl_1_1codegen_1_1_codegen_neuron_cpp_visitor.html#a8c348f1869653cec3dcc39b1866bab97", null ],
     [ "print_atomic_reduction_pragma", "classnmodl_1_1codegen_1_1_codegen_neuron_cpp_visitor.html#a910f9f22e6fbdae4eefae177eb8910f9", null ],
     [ "print_callable_preamble_from_prop", "classnmodl_1_1codegen_1_1_codegen_neuron_cpp_visitor.html#a47b63167dd62680e43439a45833b5666", null ],
-    [ "print_check_table_function_prototypes", "classnmodl_1_1codegen_1_1_codegen_neuron_cpp_visitor.html#a4ad426a930f14bc970072c5a57fb2bc9", null ],
+    [ "print_check_table_entrypoint", "classnmodl_1_1codegen_1_1_codegen_neuron_cpp_visitor.html#a389587ac35ce1f02ced547e3c8698241", null ],
     [ "print_codegen_routines", "classnmodl_1_1codegen_1_1_codegen_neuron_cpp_visitor.html#a9f9d3aebd2dc5bbc05e0cb7e9a707c7f", null ],
     [ "print_compute_functions", "classnmodl_1_1codegen_1_1_codegen_neuron_cpp_visitor.html#a94f4993226aa53f6095ad4a44fe47420", null ],
     [ "print_data_structures", "classnmodl_1_1codegen_1_1_codegen_neuron_cpp_visitor.html#aeb0635e7e6e488519882d930401b5c2b", null ],
