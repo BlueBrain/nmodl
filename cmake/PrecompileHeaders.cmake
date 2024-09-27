86cfddcd65359d86ad53392ac610775d61f7e386
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-09-27 17:02:27 +0200

    Don't precompile <string> <filesystem>

--- cmake/PrecompileHeaders.cmake
+++ cmake/PrecompileHeaders.cmake
@@ -1,7 +1,7 @@
 function(target_precompile_nmodl_headers target_name)
   target_precompile_headers(${target_name}
-    PRIVATE <string>
-    PRIVATE <filesystem>
+    # PRIVATE <string>
+    # PRIVATE <filesystem>
     PRIVATE <utils/logger.hpp>
     PRIVATE <ast/ast.hpp>
     PRIVATE <ast/all.hpp>)
