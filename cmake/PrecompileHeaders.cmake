0ab82cfaaa9ba67ea822cd593fd3b87fe5cafbae
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-09-30 13:50:50 +0200

    Disable precompiling headers.

--- cmake/PrecompileHeaders.cmake
+++ cmake/PrecompileHeaders.cmake
@@ -1,12 +1,12 @@
 function(target_precompile_nmodl_headers target_name)
 
   if(COMMAND target_precompile_headers)
-    target_precompile_headers(${target_name}
-      # PRIVATE <string>
-      # PRIVATE <filesystem>
-      PRIVATE <utils/logger.hpp>
-      PRIVATE <ast/ast.hpp>
-      PRIVATE <ast/all.hpp>)
+    # target_precompile_headers(${target_name}
+    #   # PRIVATE <string>
+    #   # PRIVATE <filesystem>
+    #   PRIVATE <utils/logger.hpp>
+    #   PRIVATE <ast/ast.hpp>
+    #   PRIVATE <ast/all.hpp>)
   else()
     message(WARNING "CMake doesn't support `target_precompile_headers`.")
   endif()
