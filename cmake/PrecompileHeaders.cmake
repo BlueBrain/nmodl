3b9c800023ae95264e0cb5f60bd0696622404fed
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-09-30 13:12:58 +0200

    Check if `target_precompile_header` exists.

--- cmake/PrecompileHeaders.cmake
+++ cmake/PrecompileHeaders.cmake
@@ -1,8 +1,13 @@
 function(target_precompile_nmodl_headers target_name)
-  target_precompile_headers(${target_name}
-    # PRIVATE <string>
-    # PRIVATE <filesystem>
-    PRIVATE <utils/logger.hpp>
-    PRIVATE <ast/ast.hpp>
-    PRIVATE <ast/all.hpp>)
+
+  if(COMMAND target_precompile_headers)
+    target_precompile_headers(${target_name}
+      # PRIVATE <string>
+      # PRIVATE <filesystem>
+      PRIVATE <utils/logger.hpp>
+      PRIVATE <ast/ast.hpp>
+      PRIVATE <ast/all.hpp>)
+  else()
+    message(WARNING "CMake doesn't support `target_precompile_headers`.")
+  endif()
 endfunction()
