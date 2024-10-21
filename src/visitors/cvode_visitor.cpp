5d94f7a88499d3f6c0fa35cfba407ec98314f6ee
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2024-10-21 16:02:11 +0200

    der_block(s) -> derivative_block(s)

--- src/visitors/cvode_visitor.cpp
+++ src/visitors/cvode_visitor.cpp
@@ -175,21 +175,22 @@ class StiffVisitor: public CvodeHelperVisitor {
 
 
 void CvodeVisitor::visit_program(ast::Program& node) {
-    auto der_blocks = collect_nodes(node, {ast::AstNodeType::DERIVATIVE_BLOCK});
-    if (!der_blocks.empty()) {
-        auto der_block = std::dynamic_pointer_cast<ast::DerivativeBlock>(der_blocks[0]);
+    auto derivative_blocks = collect_nodes(node, {ast::AstNodeType::DERIVATIVE_BLOCK});
+    if (!derivative_blocks.empty()) {
+        auto derivative_block = std::dynamic_pointer_cast<ast::DerivativeBlock>(
+            derivative_blocks[0]);
 
-        auto non_stiff_block = der_block->get_statement_block()->clone();
+        auto non_stiff_block = derivative_block->get_statement_block()->clone();
         remove_conserve_statements(*non_stiff_block);
 
-        auto stiff_block = der_block->get_statement_block()->clone();
+        auto stiff_block = derivative_block->get_statement_block()->clone();
         remove_conserve_statements(*stiff_block);
 
         NonStiffVisitor(node.get_symbol_table()).visit_statement_block(*non_stiff_block);
         StiffVisitor(node.get_symbol_table()).visit_statement_block(*stiff_block);
-        auto prime_vars = collect_nodes(*der_block, {ast::AstNodeType::PRIME_NAME});
+        auto prime_vars = collect_nodes(*derivative_block, {ast::AstNodeType::PRIME_NAME});
         node.emplace_back_node(new ast::CvodeBlock(
-            der_block->get_name(),
+            derivative_block->get_name(),
             std::shared_ptr<ast::Integer>(new ast::Integer(prime_vars.size(), nullptr)),
             std::shared_ptr<ast::StatementBlock>(non_stiff_block),
             std::shared_ptr<ast::StatementBlock>(stiff_block)));
